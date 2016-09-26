72962240028d2ec591f6b1da51ad1226fa717afc
raandree <r.andree@live.com>
2016-09-26 23:37:04 +0200

    Updating Intro Samples

--- SampleScripts/Introduction/08 Web Servers with SSL certs, Root CA, domain joined.ps1
+++ SampleScripts/Introduction/08 Web Servers with SSL certs, Root CA, domain joined.ps1
@@ -1,1 +1,27 @@
-ÿþ
+#The is almost the same like '07 Standalone Root CA, Sub Ca domain joined.ps1' but this adds a web server and requests
+#a web sever certificate for SSL.
+
+New-LabDefinition -Name 'Lab1' -DefaultVirtualizationEngine HyperV
+
+#defining default parameter values, as these ones are the same for all the machines
+$PSDefaultParameterValues = @{
+    'Add-LabMachineDefinition:DomainName' = 'contoso.com'
+    'Add-LabMachineDefinition:Memory' = 1GB
+    'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2012 R2 SERVERDATACENTER'
+}
+
+Add-LabMachineDefinition -Name DC1 -Roles RootDC
+
+Add-LabMachineDefinition -Name CA1 -Roles CaRoot
+
+Add-LabMachineDefinition -Name Web1 -Roles WebServer
+
+Add-LabMachineDefinition -Name Client1 -OperatingSystem 'Windows 10 Pro'
+
+Install-Lab
+
+Enable-LabCertificateAutoenrollment -Computer -User -CodeSigning
+
+Request-LabCertificate -Subject CN=web1.contoso.com -TemplateName WebServer -ComputerName Web1
+
+Show-LabInstallationTime
--- SampleScripts/Introduction/09 Development Client, domain joined (internet facing).ps1
+++ SampleScripts/Introduction/09 Development Client, domain joined (internet facing).ps1
@@ -1,1 +1,27 @@
-ÿþ
+#This intro script is extending '03 Single domain-joined server.ps1'. An additional ISO is added to the lab which is required to install Visual Studio 2015.
+#After the lab is installed, AutomatedLab installs Redgate Relector on the DevClient1.
+
+New-LabDefinition -Name 'Lab1' -DefaultVirtualizationEngine HyperV
+
+$labSources = Get-LabSourcesLocation
+Add-LabIsoImageDefinition -Name VisualStudio2015 -Path $labSources\ISOs\en_visual_studio_enterprise_2015_with_update_3_x86_x64_dvd_8923288.iso
+
+#defining default parameter values, as these ones are the same for all the machines
+$PSDefaultParameterValues = @{
+    'Add-LabMachineDefinition:DomainName' = 'contoso.com'
+    'Add-LabMachineDefinition:Memory' = 1GB
+    'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2012 R2 SERVERDATACENTER'
+}
+
+Add-LabMachineDefinition -Name DC1 -Roles RootDC
+
+$postInstallActivity = Get-LabPostInstallationActivity -ScriptFileName InstallSampleDBs.ps1 -DependencyFolder $labSources\PostInstallationActivities\PrepareSqlServer -KeepFolder
+Add-LabMachineDefinition -Name SQL1 -Roles SQLServer2014 -PostInstallationActivity $postInstallActivity
+
+Add-LabMachineDefinition -Name DevClient1 -OperatingSystem 'Windows 10 Pro' -Roles VisualStudio2015
+
+Install-Lab
+
+Install-LabSoftwarePackage -Path $labSources\SoftwarePackages\ReflectorInstaller.exe -CommandLine '/qn /IAgreeToTheEula' -ComputerName DevClient1
+
+Show-LabInstallationTime
