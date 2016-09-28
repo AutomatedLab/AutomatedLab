3ff688512b425ab60a2996c03b7a75198a3f2e1e
raandree <r.andree@live.com>
2016-09-28 14:17:24 +0200

    Update sample scripts

--- SampleScripts/Introduction/09 Development Client, domain joined (internet facing).ps1
+++ SampleScripts/Introduction/09 Development Client, domain joined (internet facing).ps1
@@ -1,9 +1,10 @@
-#This intro script is extending '03 Single domain-joined server.ps1'. An additional ISO is added to the lab which is required to install Visual Studio 2015.
-#After the lab is installed, AutomatedLab installs Redgate Relector on the DevClient1.
+#This intro script is extending '03 Single domain-joined server.ps1'. Two additional ISOs are added to the lab which are required to install
+#Visual Studio 2015 and SQL Server 2014. After the lab is installed, AutomatedLab installs Redgate Relector on the DevClient1.
 
 New-LabDefinition -Name 'Lab1' -DefaultVirtualizationEngine HyperV
 
 $labSources = Get-LabSourcesLocation
+Add-LabIsoImageDefinition -Name SQLServer2014 -Path $labSources\ISOs\en_sql_server_2014_standard_edition_x64_dvd_3932034.iso
 Add-LabIsoImageDefinition -Name VisualStudio2015 -Path $labSources\ISOs\en_visual_studio_enterprise_2015_with_update_3_x86_x64_dvd_8923288.iso
 
 Add-LabVirtualNetworkDefinition -Name Lab1
