08a3c8648ac9eec238de7ee7b165347ab0de598d
Raimund Andée <r.andree@live.com>
2016-10-18 11:02:37 +0200

    Adding new scenario script "DSC Pull Scenario 2 (Partial Configuration).ps1"

---
+++ SampleScripts/Scenarios/DSC Pull Scenario 2 (Partial Configuration).ps1
@@ -0,0 +1,74 @@
+$labName = 'DSCLab2'
+
+#--------------------------------------------------------------------------------------------------------------------
+#----------------------- CHANGING ANYTHING BEYOND THIS LINE SHOULD NOT BE REQUIRED ----------------------------------
+#----------------------- + EXCEPT FOR THE LINES STARTING WITH: REMOVE THE COMMENT TO --------------------------------
+#----------------------- + EXCEPT FOR THE LINES CONTAINING A PATH TO AN ISO OR APP   --------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+
+$labSources = Get-LabSourcesLocation
+
+#create an empty lab template and define where the lab XML files and the VMs will be stored
+New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV
+
+#make the network definition
+Add-LabVirtualNetworkDefinition -Name $labName
+Add-LabVirtualNetworkDefinition -Name Internet -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet' }
+
+#and the domain definition with the domain admin account
+Add-LabDomainDefinition -Name contoso.com -AdminUser install -AdminPassword Somepass1
+
+#these credentials are used for connecting to the machines. As this is a lab we use clear-text passwords
+Set-LabInstallationCredential -Username Install -Password Somepass1
+
+#defining default parameter values, as these ones are the same for all the machines
+$PSDefaultParameterValues = @{
+    'Add-LabMachineDefinition:Network' = $labName
+    'Add-LabMachineDefinition:ToolsPath'= "$labSources\Tools"
+    'Add-LabMachineDefinition:DomainName' = 'contoso.com'
+    'Add-LabMachineDefinition:Memory' = 1GB
+    'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2012 R2 SERVERDATACENTER'
+}
+
+$postInstallActivity = Get-LabPostInstallationActivity -ScriptFileName PrepareRootDomain.ps1 -DependencyFolder $labSources\PostInstallationActivities\PrepareRootDomain
+Add-LabMachineDefinition -Name DDC1 -Roles RootDC -PostInstallationActivity $postInstallActivity
+
+#the root domain gets a second domain controller
+Add-LabMachineDefinition -Name DDC2 -Roles DC
+
+#file server and router
+$netAdapter = @()
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $labName
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch Internet -UseDhcp
+Add-LabMachineDefinition -Name DRouter -Roles FileServer, Routing -NetworkAdapter $netAdapter
+
+#CA
+Add-LabMachineDefinition -Name DCA1 -Roles CaRoot
+
+#DSC Pull Servers
+Add-LabMachineDefinition -Name DPull1 -Roles DSCPullServer
+Add-LabMachineDefinition -Name DPull2 -Roles DSCPullServer
+Add-LabMachineDefinition -Name DPull3 -Roles DSCPullServer
+
+#Web Servers
+Add-LabMachineDefinition -Name DWeb1 -Roles WebServer
+Add-LabMachineDefinition -Name DWeb2 -Roles WebServer
+
+Add-LabMachineDefinition -Name DServer1
+Add-LabMachineDefinition -Name DServer2
+Add-LabMachineDefinition -Name DServer3
+
+Install-Lab
+
+#Install software to all lab machines
+$machines = Get-LabMachine
+Install-LabSoftwarePackage -ComputerName $machines -Path $labSources\SoftwarePackages\ClassicShell.exe -CommandLine '/quiet ADDLOCAL=ClassicStartMenu' -AsJob
+Install-LabSoftwarePackage -ComputerName $machines -Path $labSources\SoftwarePackages\Notepad++.exe -CommandLine /S -AsJob
+Install-LabSoftwarePackage -ComputerName $machines -Path $labSources\SoftwarePackages\winrar.exe -CommandLine /S -AsJob
+Get-Job -Name 'Installation of*' | Wait-Job | Out-Null
+
+Enable-LabCertificateAutoenrollment -Computer -User -CodeSigning
+
+Install-LabDscClient -All
+
+Show-LabInstallationTime
