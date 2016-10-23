68eaded07be6be8cc0746c0c6ec9d7b4a028ae9e
Raimund Andée <r.andree@live.com>
2016-10-24 01:04:14 +0200

    Added new validator MandatoryRoleProperties and UnknownRoleProperties

---
+++ LabXml/Validator/Roles/MandatoryRoleProperties.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomatedLab
+{
+    /// <summary>
+    /// Roles take additional properties in a hashtable. If a propery is specified but no value assigned, somthing is wrong an need to be reported.
+    /// </summary>
+    public class MandatoryRoleProperties : LabValidator, IValidate
+    {
+        public MandatoryRoleProperties()
+        {
+            messageContainer = RunValidation();
+        }
+
+        public override IEnumerable<ValidationMessage> Validate()
+        {
+            var mandatoryRoleProperties = (Hashtable)validationSettings["MandatoryRoleProperties"];
+            var machinesWithRoles = machines.Where(machine => machine.Roles.Count > 0);
+
+            foreach (var machine in machinesWithRoles)
+            {
+                foreach (var role in machine.Roles.Where(r => mandatoryRoleProperties.ContainsKey(r.Name.ToString())))
+                {
+                    foreach (string mandatoryRoleProperty in (object[])mandatoryRoleProperties[role.Name.ToString()])
+                    {
+                        if (!role.Properties.ContainsKey(mandatoryRoleProperty) || string.IsNullOrEmpty(role.Properties[mandatoryRoleProperty]))
+                        {
+                            yield return new ValidationMessage
+                            {
+                                Message = string.Format("The property '{0}' is required for role '{1}'", mandatoryRoleProperty, role.Name),
+                                Type = MessageType.Error,
+                                TargetObject = machine.Name
+                            };
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ LabXml/Validator/Roles/UnknownRoleProperties.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomatedLab
+{
+    /// <summary>
+    /// Roles take additional properties in a hashtable. If a propery is specified but no value assigned, somthing is wrong an need to be reported.
+    /// </summary>
+    public class UnknownRoleProperties : LabValidator, IValidate
+    {
+        public UnknownRoleProperties()
+        {
+            messageContainer = RunValidation();
+        }
+
+        public override IEnumerable<ValidationMessage> Validate()
+        {
+            var validRoleProperties = (Hashtable)validationSettings["ValidRoleProperties"];
+            var machinesWithRoles = machines.Where(machine => machine.Roles.Count > 0);
+
+            foreach (var machine in machinesWithRoles)
+            {
+                foreach (var role in machine.Roles.Where(r => validRoleProperties.ContainsKey(r.Name.ToString())))
+                {
+                    var unknownProperties = role.Properties.Keys.Where(p =>
+                        ((object[])validRoleProperties[role.Name.ToString()])
+                        .Cast<string>()
+                        .Where(vp => vp == p).Count() == 0);
+
+                    foreach (var unknownProperty in unknownProperties)
+                    {
+                        yield return new ValidationMessage
+                        {
+                            Message = string.Format("The property '{0}' is unknwon for role '{1}'", unknownProperty, role.Name),
+                            Type = MessageType.Error,
+                            TargetObject = machine.Name
+                        };
+                    }
+                }
+            }
+
+        }
+    }
+}
