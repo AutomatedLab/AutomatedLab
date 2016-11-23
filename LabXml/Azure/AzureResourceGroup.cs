f4306b2949e04b05f018723733f2d88bd52db604
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2016-11-23 20:34:24 +0100

    AzureResourceGroup now also holds a serializabledictionary with tags

--- LabXml/Azure/AzureResourceGroup.cs
+++ LabXml/Azure/AzureResourceGroup.cs
@@ -11,6 +11,7 @@ namespace AutomatedLab.Azure
         public string ProvisioningState { get; set; }
         public string ResourceId { get; set; }
         public string TagsTable { get; set; }
+        public SerializableDictionary<string,string> Tags { get; set; }
 
         public AzureResourceGroup()
         { }
