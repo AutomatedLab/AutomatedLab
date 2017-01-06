8ad6b8a4fe65e8dd82b8a1e6c87d37bc979551bf
Raimund Andée <raandree@live.com>
2017-01-06 01:14:34 +0100

    Increasing revision to 3.9.0.1

--- LabXml/Properties/AssemblyInfo.cs
+++ LabXml/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Raimund Andree")]
 [assembly: AssemblyProduct("LabXmlValidator")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.9.0.0")]
-[assembly: AssemblyFileVersion("3.9.0.0")]
+[assembly: AssemblyVersion("3.9.0.1")]
+[assembly: AssemblyFileVersion("3.9.0.1")]
