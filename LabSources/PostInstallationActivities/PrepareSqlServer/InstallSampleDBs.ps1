5143b04dad160535bd3e8caa185cdeee50f74dd0
Raimund Andree <r.andree@live.com>
2017-01-11 00:46:14 +0100

    This script did not work on SQL 2008 R2, fixed that

--- LabSources/PostInstallationActivities/PrepareSqlServer/InstallSampleDBs.ps1
+++ LabSources/PostInstallationActivities/PrepareSqlServer/InstallSampleDBs.ps1
@@ -1,3 +1,15 @@
-﻿Import-Module SQLPS
+﻿if (Get-Module -ListAvailable -Name SQLPS)
+{
+    Import-Module -Name SQLPS -ErrorAction SilentlyContinue
+}
+elseif (Get-PSSnapin -Registered -Name SqlServerCmdletSnapin100 -ErrorAction SilentlyContinue)
+{
+    Add-PSSnapin -Name SqlServerCmdletSnapin100 -ErrorAction SilentlyContinue
+}
+else
+{
+    throw 'Could neither add SQLPS module nor SqlServerCmdletSnapin100 snapin as they are not available'
+}
+
 Invoke-Sqlcmd -InputFile C:\PrepareSqlServer\instnwnd.sql -ServerInstance $env:COMPUTERNAME
 Invoke-Sqlcmd -InputFile C:\PrepareSqlServer\instpubs.sql -ServerInstance $env:COMPUTERNAME
