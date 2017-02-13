659442352a51613678cec426a92d9243226068b9
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-02-13 17:46:17 +0100

    Class AzureAvailabilitySet added to project definition

---
+++ LabXml/Azure/AzureAvailabilitySet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedLab.Azure
+{
+    public class AzureAvailabilitySet
+    {
+        public int Id { get; set; }
+
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public int? PlatformFaultDomainCount { get; set; }
+        public int? PlatformUpdateDomainCount { get; set; }
+        public string ResourceGroupName { get; set; }
+    }
+}
