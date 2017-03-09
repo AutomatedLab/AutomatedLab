2fe9583b1b55b706fb8148cdc9905e2fcaa5133a
Raimund Andée [MSFT] <raandree@live.com>
2017-03-09 16:23:08 +0100

    Adding support for installing a DSC Pull server without the requirement of having the lab VM connected to the internet

--- AutomatedLab/AutomatedLabDsc.psm1
+++ AutomatedLab/AutomatedLabDsc.psm1
@@ -1,7 +1,7 @@
 ﻿#region Install-LabDscPullServer
 function Install-LabDscPullServer
 {
-	# .ExternalHelp AutomatedLab.Help.xml
+    # .ExternalHelp AutomatedLab.Help.xml
     [cmdletBinding()]
     param (
         [int]$InstallationTimeout = 15
@@ -9,57 +9,73 @@ function Install-LabDscPullServer
     
     Write-LogFunctionEntry
     
+    $online = $true
     $lab = Get-Lab
     $roleName = [AutomatedLab.Roles]::DSCPullServer
+    $requiredModules = 'xPSDesiredStateConfiguration', 'xDscDiagnostics'
     
-    if (-not (Get-LabMachine))
+    if (-not (Get-LabVM))
     {
-        Write-Warning -Message 'No machine definitions imported, so there is nothing to do. Please use Import-Lab first'
+        Write-ScreenInfo -Message 'No machine definitions imported, so there is nothing to do. Please use Import-Lab first'
         Write-LogFunctionExit
         return
     }
-    
-    if (-not (Get-LabMachine -Role Routing) -and $lab.DefaultVirtualizationEngine -eq 'HyperV')
-    {
-        Write-Error 'This role requires the lab to have an internet connection. However there is no machine with the Routing role in the lab. Please make sure that one machine has also an internet facing network adapter and the routing role.'
-        return
-    }
-    
-    $machines = Get-LabMachine -Role $roleName    
+
+    $machines = Get-LabVM -Role $roleName    
     if (-not $machines)
     {
+        Write-ScreenInfo -Message 'No DSC Pull Server defined in this lab, so there is nothing to do'
+        Write-LogFunctionExit
         return
     }
+
+       
+    if (-not (Get-LabVM -Role Routing) -and $lab.DefaultVirtualizationEngine -eq 'HyperV')
+    {
+        Write-ScreenInfo 'Routing Role not detected, installing DSC in offline mode.'
+        $online = $false
+    }
+    else
+    {
+        Write-ScreenInfo 'Routing Role detected, installing DSC in online mode.'
+    }
+
+    if ($online)
+    {
+        $machinesOnline = $machines | ForEach-Object {
+            Test-LabMachineInternetConnectivity -ComputerName $_ -AsJob
+        } |
+        Receive-Job -Wait -AutoRemoveJob |
+        Where-Object { $_.TcpTestSucceeded } |
+        ForEach-Object { $_.NetAdapter.SystemName }
+
+        #if there are machines online, get the ones that are offline
+        if ($machinesOnline)
+        {
+            $machinesOffline = (Compare-Object -ReferenceObject $machines.FQDN -DifferenceObject $machinesOnline).InputObject
+        }
     
-    $machinesOnline = $machines | ForEach-Object {
-        Test-LabMachineInternetConnectivity -ComputerName $_ -AsJob
-    } |
-    Receive-Job -Wait -AutoRemoveJob |
-    Where-Object { $_.TcpTestSucceeded } |
-    ForEach-Object { $_.NetAdapter.SystemName }
+        #if there are machines offline or all machines are offline
+        if ($machinesOffline -or -not $machinesOnline)
+        {
+            Write-Error "The machines $($machinesOffline -join ', ') are not connected to the internet. Switching to offline mode."
+            $online = $false
+        }
+        else
+        {
+            Write-ScreenInfo 'All DSC Pull Servers can reach the internet.'
+        }
+    }
     
     $wrongPsVersion = Invoke-LabCommand -ComputerName $machines -ScriptBlock {
         $PSVersionTable | Add-Member -Name ComputerName -MemberType NoteProperty -Value $env:COMPUTERNAME -PassThru -Force
-    } -PassThru |
+    } -PassThru -NoDisplay |
     Where-Object { $_.PSVersion.Major -lt 5 } |
     Select-Object -ExpandProperty ComputerName
     
     if ($wrongPsVersion)
     {
         Write-Error "The following machines have an unsupported PowerShell version. At least PowerShell 5.0 is required. $($wrongPsVersion -join ', ')"
-        return
-    }
-    
-    #if there are machines online, get the ones that are offline
-    if ($machinesOnline)
-    {
-        $machinesOffline = (Compare-Object -ReferenceObject $machines.FQDN -DifferenceObject $machinesOnline).InputObject
-    }
-    
-    #if there are machines offline or all machines are offline
-    if ($machinesOffline -or -not $machinesOnline)
-    {
-        Write-Error "The machines $($machinesOffline -join ', ') are not connected to the internet. Internet connectivity is required to install DSC. Check the configuration on the machines and the machine with the Routing role."
         return
     }
     
@@ -78,27 +94,39 @@ function Install-LabDscPullServer
         New-LabCATemplate -TemplateName DscPullSsl -DisplayName 'Dsc Pull Sever SSL' -SourceTemplateName WebServer -ApplicationPolicy ServerAuthentication `
         -EnrollmentFlags Autoenrollment -PrivateKeyFlags AllowKeyExport -Version 2 -SamAccountName 'Domain Computers' -ComputerName $ca -ErrorAction Stop
     }
+
+    if ($Online)
+    {        
+        Invoke-LabCommand -ActivityName 'Setup Dsc Pull Server 1' -ComputerName $machines -ScriptBlock {
+            Install-WindowsFeature -Name DSC-Service
+            Install-PackageProvider -Name NuGet -Force
+            Install-Module -Name $using:requiredModules -Force
+        } -AsJob -PassThru | Receive-Job -AutoRemoveJob -Wait | Out-Null #only interested in errors
+    }
+    else
+    {
+        Write-ScreenInfo "Downloading the modules '$($requiredModules -join ', ')' locally and copying them to the DSC Pull Servers."
         
-    Invoke-LabCommand -ActivityName 'Setup Dsc Pull Server 1' -ComputerName $machines -ScriptBlock {
-        Install-WindowsFeature -Name DSC-Service
-        Install-PackageProvider -Name NuGet -Force
-        Install-Module xPSDesiredStateConfiguration, xDscDiagnostics -Force            
-    } -AsJob -PassThru | Receive-Job -AutoRemoveJob -Wait | Out-Null #only interested in errors
+        Install-PackageProvider -Name NuGet -Force | Out-Null
+        Install-Module -Name $requiredModules -Force
+
+        $modulePaths = Get-Module -Name $requiredModules -ListAvailable | Select-Object -ExpandProperty ModuleBase | ForEach-Object { Split-Path -Path $_ -Parent }
+        Copy-LabFileItem -Path $modulePaths -ComputerName $machines -DestinationFolder 'C:\Program Files\WindowsPowerShell\Modules'
+    }
     
     Copy-LabFileItem -Path $labSources\PostInstallationActivities\SetupDscPullServer\SetupDscPullServer.ps1,
     $labSources\PostInstallationActivities\SetupDscPullServer\DscTestConfig.ps1 -ComputerName $machines
+
+    $dscResources = Get-Module -ListAvailable | Where-Object { $_.Tags -contains 'DSCResource' -and $_.Name -notin $requiredModules }
+    Write-ScreenInfo "Publishing local DSC resources: $($dscResources.Name -join ', ')..." -NoNewLine
+    $modulePaths = $dscResources | Select-Object -ExpandProperty ModuleBase | ForEach-Object { Split-Path -Path $_ -Parent }
+    Copy-LabFileItem -Path $modulePaths -ComputerName $machines -DestinationFolder 'C:\Program Files\WindowsPowerShell\Modules'
+    Write-ScreenInfo 'finished'
 
     $jobs = @()
 
     foreach ($machine in $machines)
     {
-        $psVersion = Invoke-LabCommand -ActivityName 'Get PowerShell Version' -ComputerName $machine -ScriptBlock { $PSVersionTable } -PassThru
-        if (-not $psVersion.PSVersion -ge '5.0')
-        {
-            Write-Error 'The DSC Pull Server role requires at least PowerShell 5.0. Please install it before installing this role.'
-            continue
-        }
-        
         $cert = Request-LabCertificate -Subject "CN=*.$($machine.DomainName)" -TemplateName DscPullSsl -ComputerName $machine -PassThru -ErrorAction Stop
         
         $guid = (New-Guid).Guid
@@ -146,7 +174,7 @@ function Install-LabDscPullServer
 #region Install-LabDscClient
 function Install-LabDscClient
 {
-	# .ExternalHelp AutomatedLab.Help.xml
+    # .ExternalHelp AutomatedLab.Help.xml
     [CmdletBinding(DefaultParameterSetName = 'ByName')]
     param(
         [Parameter(Mandatory, ParameterSetName = 'ByName')]
@@ -160,11 +188,11 @@ function Install-LabDscClient
     
     if ($All)
     {
-        $machines = Get-LabMachine | Where-Object { $_.Roles.Name -notin 'DC', 'RootDC', 'FirstChildDC', 'DSCPullServer' }
+        $machines = Get-LabVM | Where-Object { $_.Roles.Name -notin 'DC', 'RootDC', 'FirstChildDC', 'DSCPullServer' }
     }
     else
     {
-        $machines = Get-LabMachine -ComputerName $ComputerName
+        $machines = Get-LabVM -ComputerName $ComputerName
     }
     
     if (-not $machines)
@@ -177,19 +205,19 @@ function Install-LabDscClient
     
     if ($PullServer)
     {
-        if (-not (Get-LabMachine -ComputerName $PullServer | Where-Object { $_.Roles.Name -contains 'DSCPullServer' }))
+        if (-not (Get-LabVM -ComputerName $PullServer | Where-Object { $_.Roles.Name -contains 'DSCPullServer' }))
         {
             Write-Error "The given DSC Pull Server '$PullServer' could not be found in the lab."
             return
         }
         else
         {
-            $pullServerMachines = Get-LabMachine -ComputerName $PullServer
+            $pullServerMachines = Get-LabVM -ComputerName $PullServer
         }
     }
     else
     {
-        $pullServerMachines = Get-LabMachine -Role DSCPullServer
+        $pullServerMachines = Get-LabVM -Role DSCPullServer
     }
     
     Copy-LabFileItem -Path $labSources\PostInstallationActivities\SetupDscClients\SetupDscClients.ps1 -ComputerName $machines
