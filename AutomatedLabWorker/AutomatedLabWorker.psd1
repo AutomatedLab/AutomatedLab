7dea5b12e33565703b9c7cbd10561644850b9c29
Raimund Andée [MSFT] <raandree@live.com>
2017-03-31 18:27:33 +0200

    Increasing build number

--- AutomatedLabWorker/AutomatedLabWorker.psd1
+++ AutomatedLabWorker/AutomatedLabWorker.psd1
@@ -1,7 +1,7 @@
 @{
     RootModule = 'AutomatedLabWorker.psm1'
     
-    ModuleVersion = '4.0.0.2'
+    ModuleVersion = '4.0.1.0'
     
     GUID = '3addac35-cd7a-4bd2-82f5-ab9c83a48246'
     
--- LabXml/Properties/AssemblyInfo.cs
+++ LabXml/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.2")]
-[assembly: AssemblyFileVersion("4.0.0.2")]
+[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
