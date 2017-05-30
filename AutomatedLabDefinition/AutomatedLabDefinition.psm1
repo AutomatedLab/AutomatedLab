c14a2010e1c1395cd7203deecb0f8d9ab2071c59
Jawz84 <joskw@live.com>
2017-05-30 11:26:23 +0200

    Basic modifications to enable VMWare as DefaultVirtualizationEngine

--- AutomatedLabDefinition/AutomatedLabDefinition.psm1
+++ AutomatedLabDefinition/AutomatedLabDefinition.psm1
@@ -543,7 +543,7 @@ function New-LabDefinition
 
         [switch]$UseStaticMemory = $false,
 
-        [ValidateSet('Azure', 'HyperV')]
+        [ValidateSet('Azure', 'HyperV', 'VMWare')]
         [string]$DefaultVirtualizationEngine,
         
         [switch]$NoAzurePublishSettingsFile,
@@ -2498,6 +2498,10 @@ process
     {
         $machine.OperatingSystem = $OperatingSystem
     }
+    elseif ($machine.HostType -eq 'VMWare') 
+    {
+        $machine.OperatingSystem = $OperatingSystem
+    }
     
     if (-not $TimeZone)   { $TimeZone = tzutil.exe /g }
     $machine.Timezone = $TimeZone
@@ -2798,29 +2802,18 @@ function Get-DiskSpeed
         $labSources = Get-LabSourcesLocation
     }
 
-    $IsReadOnly = Get-Partition -DriveLetter ($DriveLetter.TrimEnd(':')) | Select-Object -ExpandProperty IsReadOnly
-    if ($IsReadOnly)
-    {
-        Write-ScreenInfo -Message "Drive $DriveLetter is read-only. Skipping disk speed test" -Type Warning
-
-        $readThroughoutRandom = 0
-        $writeThroughoutRandom = 0
-    }
-    else
-    {
-        Write-ScreenInfo -Message "Measuring speed of drive $DriveLetter" -Type Info
+    Write-ScreenInfo -Message "Measuring speed of drive $DriveLetter" -Type Info
     
-        $tempFileName = [System.IO.Path]::GetTempFileName()
+    $tempFileName = [System.IO.Path]::GetTempFileName()
     
-        & "$labSources\Tools\WinSAT.exe" disk -ran -read -count $Interations -drive $DriveLetter -xml $tempFileName | Out-Null
-        $readThroughoutRandom = (Select-Xml -Path $tempFileName -XPath '/WinSAT/Metrics/DiskMetrics/AvgThroughput').Node.'#text'
+    & "$labSources\Tools\WinSAT.exe" disk -ran -read -count $Interations -drive $DriveLetter -xml $tempFileName | Out-Null
+    $readThroughoutRandom = (Select-Xml -Path $tempFileName -XPath '/WinSAT/Metrics/DiskMetrics/AvgThroughput').Node.'#text'
     
-        & "$labSources\Tools\WinSAT.exe" disk -ran -write -count $Interations -drive $DriveLetter -xml $tempFileName | Out-Null
-        $writeThroughoutRandom = (Select-Xml -Path $tempFileName -XPath '/WinSAT/Metrics/DiskMetrics/AvgThroughput').Node.'#text'
+    & "$labSources\Tools\WinSAT.exe" disk -ran -write -count $Interations -drive $DriveLetter -xml $tempFileName | Out-Null
+    $writeThroughoutRandom = (Select-Xml -Path $tempFileName -XPath '/WinSAT/Metrics/DiskMetrics/AvgThroughput').Node.'#text'
     
-        Remove-Item -Path $tempFileName
-    }
-
+    Remove-Item -Path $tempFileName
+    
     $result = New-Object PSObject -Property ([ordered]@{
             ReadRandom = $readThroughoutRandom
             WriteRandom = $writeThroughoutRandom
