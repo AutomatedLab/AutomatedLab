a4d86793bb15a20bd9b22449b4912bd104d99aad
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-08-04 18:43:11 +0200

    Subnet validators added/updated

---
+++ LabXml/Validator/Network/Azure Network/AzureVnetAddressSpaceTooSmall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedLab.Validator.Network.Azure_Network
+{
+    public class AzureVnetAddressSpaceTooSmall : LabValidator, IValidate
+    {
+        public AzureVnetAddressSpaceTooSmall()
+        {
+            messageContainer = RunValidation();
+        }
+
+        public override IEnumerable<ValidationMessage> Validate()
+        {
+            var vnets = lab.VirtualNetworks.Where(adapter => adapter.HostType == VirtualizationHost.Azure && adapter.Subnets.Count > 0);
+            if (vnets.Count() == 0)
+                yield break;
+
+            foreach (var vnet in vnets)
+            {
+                if (vnet.Subnets.Count > 1 && vnet.Subnets.Where(sn => sn.AddressSpace.Cidr >= vnet.AddressSpace.Cidr).Count() > 0 ||
+                      vnet.Subnets.Count == 1 && vnet.Subnets.Where(sn => sn.AddressSpace.Cidr > vnet.AddressSpace.Cidr).Count() > 0)
+                {
+                    yield return new ValidationMessage
+                    {
+                        Message = "At least one subnet's address space is bigger or equal to the virtual network's address space.",
+                        HelpText = "Reexamine the CIDR of your Azure virtual network and the subnets you have configured. If you configure more than one subnet, make sure that the address space fits your subnets.",
+                    };
+                }
+            }
+        }
+    }
+}
