46d23e017339a8f4b90f769238e8b673b4058fca
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-08-16 17:44:31 +0200

    Formating, renamed toast application to AutomatedLab

--- AutomatedLab/AutomatedLabInternals.psm1
+++ AutomatedLab/AutomatedLabInternals.psm1
@@ -2225,37 +2225,37 @@ function Test-HashtableKeys
 
 function Show-LabToastNotification
 {
-param
-(
-	[Parameter(Mandatory = $true, ParameterSetName = 'Start')]
-	[switch]
-	$LabStarted,
+    param
+    (
+        [Parameter(Mandatory = $true, ParameterSetName = 'Start')]
+        [switch]
+        $LabStarted,
 
-	[Parameter(Mandatory = $true, ParameterSetName = 'Finish')]
-	[switch]
-	$LabFinished
-)
+        [Parameter(Mandatory = $true, ParameterSetName = 'Finish')]
+        [switch]
+        $LabFinished
+    )
 
-$lab = Get-Lab -ErrorAction SilentlyContinue
+    $lab = Get-Lab -ErrorAction SilentlyContinue
 
-if (-not $lab)
-{
-	return
-}
+    if (-not $lab)
+    {
+        return
+    }
 
-$status = if($LabStarted){"started"}else{"finished"}
-$template = "<toast><visual><binding template=`"ToastText02`"><text id=`"1`">AutomatedLab</text><text id=`"2`">Deployment of {0} on {1} {2}.</text></binding></visual></toast>" -f `
-	$lab.Name,$lab.DefaultVirtualizationEngine,$status
+    $status = if ($LabStarted) {"started"}else {"finished"}
+    $template = "<toast><visual><binding template=`"ToastText02`"><text id=`"1`">AutomatedLab</text><text id=`"2`">Deployment of {0} on {1} {2}.</text></binding></visual></toast>" -f `
+        $lab.Name, $lab.DefaultVirtualizationEngine, $status
 
 
-[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
-[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] > $null
-[Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
-$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+    [void] ([Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime])
+    [void] ([Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime])
+    [void] ([Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime])
+    $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
 
-$xml.LoadXml($template)
-$toast = New-Object Windows.UI.Notifications.ToastNotification $xml
-[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("AL").Show($toast)
+    $xml.LoadXml($template)
+    $toast = New-Object Windows.UI.Notifications.ToastNotification $xml
+    [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("AL").Show($toast)
 }
 
 Add-Type -TypeDefinition $meshType
