2381b36e2df81039d07e66a141cbd896589ba3d8
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-08-25 19:37:12 +0200

    Invoke-WebRequest ignores ErrorAction

--- AutomatedLabNotifications/Private/Send-ALIftttNotification.ps1
+++ AutomatedLabNotifications/Private/Send-ALIftttNotification.ps1
@@ -21,10 +21,18 @@ function Send-ALIftttNotification
         value2 = $Activity
         value3 = $Message
     }
-    $request = Invoke-WebRequest -Method Post -Uri https://maker.ifttt.com/trigger/$($eventName)/with/key/$($key) -ContentType "application/json" -Body ($messageBody | ConvertTo-Json -Compress) -ErrorAction SilentlyContinue
 
-    if (-not $request.StatusCode -eq 200)
+    try 
     {
-        Write-Verbose -Message "Notification to IFTTT could not be sent with event $eventName. Status code was $($request.StatusCode)"
+        $request = Invoke-WebRequest -Method Post -Uri https://maker.ifttt.com/trigger/$($eventName)/with/key/$($key) -ContentType "application/json" -Body ($messageBody | ConvertTo-Json -Compress) -ErrorAction Stop
+        
+        if (-not $request.StatusCode -eq 200)
+        {
+            Write-Verbose -Message "Notification to IFTTT could not be sent with event $eventName. Status code was $($request.StatusCode)"
+        }
     }
+    catch 
+    {
+        Write-Verbose -Message "Notification to IFTTT could not be sent with event $eventName."
+    }    
 }
