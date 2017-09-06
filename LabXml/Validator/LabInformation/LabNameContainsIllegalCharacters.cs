aa85ca5034f7da8cbc0cdee03c30560fc31eb996
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-09-06 09:45:46 +0200

    Validator updated
    fixes #149
    Using Equals for string comparison

--- LabXml/Validator/LabInformation/LabNameContainsIllegalCharacters.cs
+++ LabXml/Validator/LabInformation/LabNameContainsIllegalCharacters.cs
@@ -18,12 +18,20 @@ namespace AutomatedLab
             var pattern = "^([A-Za-z0-9])+$";
             var azurePattern = "^([A-Za-z0-9-_.])+(?<!\\.)$";
 
-            if (!System.Text.RegularExpressions.Regex.IsMatch(lab.Name, pattern) ||
-                lab.DefaultVirtualizationEngine != "Azure" && !System.Text.RegularExpressions.Regex.IsMatch(lab.Name, azurePattern))
             {
                 yield return new ValidationMessage()
                 {
-                    Message = $"The lab name contains invalid characters. Only A-Z, a-z and 0-9 are allowed. {azurePattern} if you are on Azure.",
+                    Message = $"The lab name contains invalid characters. Only names matching {azurePattern} are allowed.",
+                    TargetObject = lab.Name,
+                    Type = MessageType.Error,
+                };
+            }
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(lab.Name, pattern))
+            {
+                yield return new ValidationMessage()
+                {
+                    Message = $"The lab name contains invalid characters. Only A-Z, a-z and 0-9 are allowed.",
                     TargetObject = lab.Name,
                     Type = MessageType.Error,
                 };
