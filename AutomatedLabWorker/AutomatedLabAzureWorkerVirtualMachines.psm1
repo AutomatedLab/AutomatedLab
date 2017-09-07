32b410beebf204252111b5a52364f2d0489c9c13
Raimund Andée [MSFT] <raandree@live.com>
2017-09-07 23:28:35 +0200

    Fixing a bug in Mount-LWAzureIsoImage, the DriveLetter did not have a : at the end

--- AutomatedLabWorker/AutomatedLabAzureWorkerVirtualMachines.psm1
+++ AutomatedLabWorker/AutomatedLabAzureWorkerVirtualMachines.psm1
@@ -1500,7 +1500,9 @@ function Mount-LWAzureIsoImage
     $azureIsoPath = $IsoPath -replace '/', '\' -replace 'https:'
 
     Invoke-LabCommand -ActivityName "Mounting $(Split-Path $azureIsoPath -Leaf) on $($ComputerName.Name -join ',')" -ComputerName $ComputerName -ScriptBlock {
-        Mount-DiskImage -ImagePath $args[0] -StorageType ISO -PassThru | Get-Volume
+        $drive = Mount-DiskImage -ImagePath $args[0] -StorageType ISO -PassThru | Get-Volume
+		$drive | Add-Member -MemberType NoteProperty -Name DriveLetter -Value ($drive.CimInstanceProperties.Item('DriveLetter').Value + ":") -Force
+		$drive | Select-Object -Property *
     } -ArgumentList $azureIsoPath -PassThru:$PassThru
 }
 #endregion
