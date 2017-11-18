f22b25d5cb1c960c90e85de0dcb54cbbc2f90047
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2017-11-18 19:27:21 +0100

    Failover Clustering 2008 R2 cmdlets fixed

--- AutomatedLab/AutomatedLabFailover.psm1
+++ AutomatedLab/AutomatedLabFailover.psm1
@@ -11,7 +11,7 @@ function Install-LabFailoverCluster
     $clusters = $failoverNodes | Group-Object { ($PSItem.Roles | Where-Object -Property Name -eq 'FailoverNode').Properties['ClusterName'] }
     $useDiskWitness = $false
 
-    Install-LabWindowsFeature -ComputerName $failoverNodes -FeatureName Failover-Clustering, RSAT-Clustering-PowerShell
+    Install-LabWindowsFeature -ComputerName $failoverNodes -FeatureName Failover-Clustering, RSAT-Clustering -IncludeAllSubFeature
     
     if (Get-LabVm -Role FailoverStorage)
     {
@@ -20,9 +20,7 @@ function Install-LabFailoverCluster
         $useDiskWitness = $true
     }
 
-    Write-Screeninfo -Message 'Waiting for failover nodes to complete installation'
-
-    
+    Write-Screeninfo -Message 'Waiting for failover nodes to complete installation'    
 
     foreach ($cluster in $clusters)
     {
@@ -43,7 +41,7 @@ function Install-LabFailoverCluster
             $clusterName = 'ALCluster'
         }
 
-        if ($useDiskWitness)
+        if ($useDiskWitness -and -not ($firstNode.OperatingSystem.Version -lt 6.2))
         {
             Invoke-LabCommand -ComputerName $firstNode -ActivityName 'Preparing cluster storage' -ScriptBlock {
                 if (-not (Get-ClusterAvailableDisk -ErrorAction SilentlyContinue))
@@ -61,49 +59,112 @@ function Install-LabFailoverCluster
                     }
                 }
             }
-        }
 
-        $clusterAccessPoint = if ($clusterDomains.Count -ne 1)
-        {
-            'DNS'
+            Invoke-LabCommand -ComputerName $clusterNodeNames -ActivityName 'Preparing cluster storage on remaining nodes' -ScriptBlock {
+                Get-Disk | Where-Object -Property OperationalStatus -eq Offline | Set-Disk -IsOffline $false
+            }
         }
         else
         {
-            'ActiveDirectoryAndDns'    
-        }
-        
-        Invoke-LabCommand -ComputerName $firstNode -ActivityName 'Enabling clustering on first node' -ScriptBlock {
-            $clusterParameters = @{
-                Name                      = $clusterName
-                Node                      = $env:COMPUTERNAME
-                StaticAddress             = $clusterIp
-                AdministrativeAccessPoint = $clusterAccessPoint
-                ErrorAction               = 'Stop'
-                WarningAction             = 'SilentlyContinue'
-            }
+            Invoke-LabCommand -ComputerName $firstNode -ActivityName 'Preparing cluster storage' -ScriptBlock {
+                $diskpartCmd = 'LIST DISK'
 
-            $clusterParameters = Sync-Parameter -Command (Get-Command New-Cluster) -Parameters $clusterParameters
+                $disks = $diskpartCmd | diskpart.exe
 
-            New-Cluster @clusterParameters
-
-            while (-not (Get-Cluster -Name $clusterName -ErrorAction SilentlyContinue))
-            {
-                Start-Sleep -Seconds 1
-            }
-
-            Get-Cluster -Name $clusterName | Add-ClusterNode $clusterNodeNames
-            
-            if ($useDiskWitness)
-            {
-                $clusterDisk = Get-ClusterResource -Cluster $clusterName -ErrorAction SilentlyContinue | Where-object -Property ResourceType -eq 'Physical Disk'
-
-                if ($clusterDisk)
+                foreach ($line in $disks)
                 {
-                    Get-Cluster -Name $clusterName | Set-ClusterQuorum -DiskWitness $clusterDisk
+                    if ($line -match 'Disk (?<DiskNumber>\d) \s+(Offline)\s+(?<Size>\d+) GB\s+(?<Free>\d+) GB')
+                    {
+                        $nextDriveLetter = [char[]](67..90) | 
+                            Where-Object { (Get-WmiObject -Class Win32_LogicalDisk | 
+                                    Select-Object -ExpandProperty DeviceID) -notcontains "$($_):"} | 
+                            Select-Object -First 1
+
+                        $diskNumber = $Matches.DiskNumber
+
+                        $diskpartCmd = "@
+                        SELECT DISK $diskNumber
+                        ATTRIBUTES DISK CLEAR READONLY
+                        ONLINE DISK
+                        CREATE PARTITION PRIMARY
+                        ASSIGN LETTER=$nextDriveLetter
+                        EXIT
+                    @"
+                        $diskpartCmd | diskpart.exe | Out-Null
+
+                        Start-Sleep -Seconds 2
+
+                        cmd.exe /c "echo y | format $($nextDriveLetter): /q /v:DataDisk$diskNumber"
+                    }
                 }
             }
-        } -Variable (Get-Variable clusterName, clusterNodeNames, clusterIp, useDiskWitness, clusterAccessPoint) -Function (Get-Command Sync-Parameter)
-    }    
+            
+            Invoke-LabCommand -ComputerName $clusterNodeNames -ActivityName 'Preparing cluster storage' -ScriptBlock {
+                $diskpartCmd = 'LIST DISK'
+    
+                $disks = $diskpartCmd | diskpart.exe
+    
+                foreach ($line in $disks)
+                {
+                    if ($line -match 'Disk (?<DiskNumber>\d) \s+(Offline)\s+(?<Size>\d+) GB\s+(?<Free>\d+) GB')
+                    {    
+                        $diskNumber = $Matches.DiskNumber
+    
+                        $diskpartCmd = "@
+                            SELECT DISK $diskNumber
+                            ATTRIBUTES DISK CLEAR READONLY
+                            ONLINE DISK
+                            EXIT
+                        @"
+                        $diskpartCmd | diskpart.exe | Out-Null
+                    }
+                }
+            }
+        }
+    }
+
+    $clusterAccessPoint = if ($clusterDomains.Count -ne 1)
+    {
+        'DNS'
+    }
+    else
+    {
+        'ActiveDirectoryAndDns'    
+    }
+        
+    Invoke-LabCommand -ComputerName $firstNode -ActivityName 'Enabling clustering on first node' -ScriptBlock {
+        Import-Module FailoverClusters -ErrorAction Stop
+
+        $clusterParameters = @{
+            Name                      = $clusterName
+            Node                      = $env:COMPUTERNAME
+            StaticAddress             = $clusterIp
+            AdministrativeAccessPoint = $clusterAccessPoint
+            ErrorAction               = 'Stop'
+            WarningAction             = 'SilentlyContinue'
+        }
+
+        $clusterParameters = Sync-Parameter -Command (Get-Command New-Cluster) -Parameters $clusterParameters
+
+        New-Cluster @clusterParameters
+
+        while (-not (Get-Cluster -Name $clusterName -ErrorAction SilentlyContinue))
+        {
+            Start-Sleep -Seconds 1
+        }
+
+        Get-Cluster -Name $clusterName | Add-ClusterNode $clusterNodeNames
+            
+        if ($useDiskWitness)
+        {
+            $clusterDisk = Get-ClusterResource -Cluster $clusterName -ErrorAction SilentlyContinue | Where-object -Property ResourceType -eq 'Physical Disk'
+
+            if ($clusterDisk)
+            {
+                Get-Cluster -Name $clusterName | Set-ClusterQuorum -DiskWitness $clusterDisk
+            }
+        }
+    } -Variable (Get-Variable clusterName, clusterNodeNames, clusterIp, useDiskWitness, clusterAccessPoint) -Function (Get-Command Sync-Parameter)
 }
 #endregion
 
@@ -191,8 +252,17 @@ function Install-LabFailoverStorage
     $targetAddress = $storageNode.IpV4Address
 
     Invoke-LabCommand -ActivityName 'Connecting iSCSI target' -ComputerName (Get-LabVm -Role FailoverNode) -ScriptBlock {
-        New-IscsiTargetPortal -TargetPortalAddress $targetAddress
-        Get-IscsiTarget | Where-Object {-not $PSItem.IsConnected} | Connect-IscsiTarget -IsPersistent $true
+        if (-not (Get-Command New-IscsiTargetPortal))
+        {
+            iscsicli.exe QAddTargetPortal $targetAddress
+            $target = ((iscsicli.exe ListTargets) -match 'iqn.+target')[0].Trim()
+            iscsicli.exe QLoginTarget $target
+        }
+        else
+        {
+            New-IscsiTargetPortal -TargetPortalAddress $targetAddress
+            Get-IscsiTarget | Where-Object {-not $PSItem.IsConnected} | Connect-IscsiTarget -IsPersistent $true    
+        }        
     } -Variable (Get-Variable targetAddress) -ErrorAction Stop
 }
 #endregion
