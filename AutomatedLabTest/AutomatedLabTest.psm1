6bd569765492ed58456592f2bf9dee144cbc1682
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2017-12-07 10:50:42 +0100

    Moved several functions to AutomatedLab.Common

--- AutomatedLabTest/AutomatedLabTest.psm1
+++ AutomatedLabTest/AutomatedLabTest.psm1
@@ -1,42 +1,5 @@
 ﻿$testResultPattern = '\d{6}_\d{4}_([\w\(\),-]| )+_Log.xml'
 
-#region Get-ConsoleText
-function Get-ConsoleText
-{
-    # Check the host name and exit if the host is not the Windows PowerShell console host. 
-    if ($host.Name -eq 'Windows PowerShell ISE Host')
-    { 
-        $psISE.CurrentPowerShellTab.ConsolePane.Text
-    }
-    elseif ($host.Name -eq 'ConsoleHost')
-    {
-        $textBuilderConsole = New-Object System.Text.StringBuilder
-        $textBuilderLine = New-Object System.Text.StringBuilder
-
-        # Grab the console screen buffer contents using the Host console API.
-        $bufferWidth = $host.UI.RawUI.BufferSize.Width
-        $bufferHeight = $host.UI.RawUI.CursorPosition.Y 
-        $rec = New-Object System.Management.Automation.Host.Rectangle(0,0,($bufferWidth),$bufferHeight)
-        $buffer = $host.UI.RawUI.GetBufferContents($rec) 
-
-        # Iterate through the lines in the console buffer. 
-        for($i = 0; $i -lt $bufferHeight; $i++) 
-        { 
-            for($j = 0; $j -lt $bufferWidth; $j++) 
-            { 
-                $cell = $buffer[$i,$j] 
-                $null = $textBuilderLine.Append($cell.Character)
-            }
-            $null = $textBuilderConsole.AppendLine($textBuilderLine.ToString().TrimEnd())
-            $textBuilderLine = New-Object System.Text.StringBuilder
-        }
-
-        $textBuilderConsole.ToString()
-        Write-Verbose "$bufferHeight lines have been copied to the clipboard"
-    }
-}
-#endregion Get-ConsoleText
-
 #region Test-LabDeployment
 function Test-LabDeployment
 {
