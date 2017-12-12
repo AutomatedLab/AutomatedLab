d43e7b22c6d475f06184a64154fb3520a1040643
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2017-12-12 17:25:02 +0100

    Prep for automated build

--- automatedlab.psdeploy.ps1
+++ automatedlab.psdeploy.ps1
@@ -1,31 +1,28 @@
-# Generic module deployment.
-# This stuff should be moved to psake for a cleaner deployment view
-
-# ASSUMPTIONS:
-
-# folder structure of:
-# - RepoFolder
-#   - This PSDeploy file
-#   - ModuleName
-#     - ModuleName.psd1
-
-# Nuget key in $ENV:NugetApiKey
-
-# Set-BuildEnvironment from BuildHelpers module has populated ENV:BHProjectName
+$moduleNames = @(
+    'AutomatedLab'
+    'AutomatedLabDefinition'
+    'AutomatedLabNotifications'
+    'AutomatedLabTest'
+    'AutomatedLabUnattended'
+    'AutomatedLabWorker'
+    'HostsFile'
+    'PSLog'
+)
 
 # Publish to gallery with a few restrictions
-if (
-    (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -and
-    $env:BHBuildSystem -ne 'Unknown' -and
-    $env:BHBranchName -eq "master"
+if ($true
+    #$env:APPVEYOR_REPO_BRANCH -eq "master"
 )
 {
-    Deploy Module {
-        By PSGalleryModule {
-            FromSource (Join-Path $ENV:BHProjectPath $ENV:BHProjectName)
-            To PSGallery
-            WithOptions @{
-                ApiKey = $ENV:NugetApiKey
+    foreach ($moduleName in $moduleNames)
+    {
+        Deploy Module {
+            By PSGalleryModule {
+                FromSource (Join-Path $PSScriptRoot $moduleName)
+                To PSGallery
+                WithOptions @{
+                    ApiKey = $ENV:NugetApiKey
+                }
             }
         }
     }
@@ -41,17 +38,19 @@ else
 
 # Publish to AppVeyor if we're in AppVeyor
 if (
-    (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -and
-    $env:BHBuildSystem -eq 'AppVeyor'
+    $env:APPVEYOR_BUILD_VERSION
 )
 {
+    foreach ($moduleName in $moduleNames)
+    {
     Deploy DeveloperBuild {
         By AppVeyorModule {
-            FromSource (Join-Path $ENV:BHProjectPath $ENV:BHProjectName)
+            FromSource (Join-Path $PSScriptRoot $moduleName)
             To AppVeyor
             WithOptions @{
                 Version = $env:APPVEYOR_BUILD_VERSION
             }
         }
     }
+}
 }
