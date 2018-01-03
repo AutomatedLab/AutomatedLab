6e38ee86305072881d457acaf24e5a4f287675c5
Raimund Andée [MSFT] <raandree@live.com>
2018-01-03 13:59:02 +0100

    Adding samples for nested virtualization of HyperV and Azure. This fixes #189

---
+++ LabSources/SampleScripts/Scenarios/Lab in a Box 2 - Azure.ps1
@@ -0,0 +1,85 @@
+<#
+In this scenario AutomatedLab builds a lab inside a lab. Thanks to nested virtualization in Hyper-V and Azure,
+this can be done on a Windows Server 2016 or Windows 10 host machine.
+This lab contains:
+    -	ADDC1 with the role root domain controller
+    -	AL1, the virtualized host machine on Windows Server Core 1709.
+    -	optional AL2, another virtualized host machine on Windows Server 2016 with GUI. This machine also has the
+            routing role to enable internet access for the whole lab.
+    -	ADClient1 gives you graphical management access to the virtualized host.
+
+Note: The domain controller and client are not required. These machine just add another level of comfort to have
+graphical management of the virtual host machine and the lab inside.
+
+After AutomatedLab has created the machines, it installs the Hyper-V roles on AL1 (and AL2) and ALClient1. Then the AutomatedLab
+PowerShell modules are downloaded and installed on the virtual hosts. The ISO files are downloaded from the Azure LabSources folder
+to the virtual hosts. If you have not synces it with your local LabSources folder, just call
+"Sync-LabAzureLabSources -DoNotSkipOsIsos" to have your OS ISO images on Azure as well. ISOs on AL1 in order to deploy a lab 
+on the virtualized host so AL copied some files to the virtual host. Finally, the deployment script calls the sample script
+"04 Single domain-joined server.ps1" on AL1 and deploys a lab in a lab.
+#>
+
+$azureResourceManagerProfile = '<PATH TO YOUR AZURE RM PROFILE>' #IF YOU HAVE NO PROFILE FILE, CALL Save-AzureRmContext
+$azureDefaultLocation = 'West Europe' #COMMENT OUT -DefaultLocationName BELOW TO USE THE FASTEST LOCATION
+
+$labName = 'ALTestLab2'
+
+New-LabDefinition -Name $labName -DefaultVirtualizationEngine Azure
+
+Add-LabAzureSubscription -Path $azureResourceManagerProfile -DefaultLocationName $azureDefaultLocation
+
+Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace 192.168.25.1/24
+
+$PSDefaultParameterValues = @{
+    'Add-LabMachineDefinition:Network' = $labName
+    'Add-LabMachineDefinition:ToolsPath'= "$labSources\Tools"
+    'Add-LabMachineDefinition:OperatingSystem'= 'Windows Server 2016 SERVERDATACENTER'
+    'Add-LabMachineDefinition:Memory'= 1GB
+    'Add-LabMachineDefinition:DomainName'= 'contoso.com'
+}
+
+Add-LabMachineDefinition -Name ALDC1 -Roles RootDC
+
+Add-LabMachineDefinition -Name AL1 -Memory 32GB -OperatingSystem 'Windows Server 2016 SERVERDATACENTERACORE'
+#Add-LabMachineDefinition -Name AL2 -Memory 32GB -OperatingSystem 'Windows Server 2016 SERVERDATACENTER'
+
+Add-LabMachineDefinition -Name ALClient1 -OperatingSystem 'Windows 10 Pro'
+
+Install-Lab
+
+$alServers = Get-LabVM | Where-Object Name -Like AL? #should be AL1 and AL2 (if available)
+$alClients = Get-LabVM -ComputerName ALCient1
+
+Install-LabWindowsFeature -ComputerName $alServers -FeatureName Hyper-V, Hyper-V-PowerShell -IncludeAllSubFeature
+Restart-LabVM -ComputerName $alServers -Wait #A restart is required by the Hyper-V installer
+
+Install-LabWindowsFeature -ComputerName ALClient1 -FeatureName Microsoft-Hyper-V-All
+Restart-LabVM -ComputerName ALClient1 -Wait
+
+Invoke-LabCommand -ActivityName 'Install AutomatedLab and create LabSources folder' -ComputerName $alServers -ScriptBlock {
+
+    Install-PackageProvider -Name Nuget -ForceBootstrap -Force -ErrorAction Stop | Out-Null
+    Install-Module -Name AutomatedLab -AllowClobber -Force -ErrorAction Stop
+
+    Import-Module -Name AutomatedLab -ErrorAction Stop
+
+    $dataVolume = Get-Volume -FileSystemLabel DataDisk* | Select-Object -First 1
+    New-LabSourcesFolder -DriveLetter $dataVolume.DriveLetter -ErrorAction Stop
+
+}
+
+Invoke-LabCommand -ActivityName 'Copy ISOs from Azure LabSources folder' -ComputerName $alServers -ScriptBlock {
+
+    Copy-Item -Path Z:\ISOs\* -Destination "$($dataVolume.DriveLetter):\LabSources\ISOs" -PassThru
+
+} -PassThru
+
+Invoke-LabCommand -ActivityName 'Deploy Test Lab' -ComputerName $alServers -ScriptBlock {
+
+    & "$(Get-LabSourcesLocation)\SampleScripts\Introduction\04 Single domain-joined server.ps1"
+
+}
+
+Stop-LabVM -All -Wait #stop and deallocate all machines for cost reasons
+
+Show-LabDeploymentSummary -Detailed
