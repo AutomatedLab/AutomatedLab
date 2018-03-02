b01a8ffb5c1929d5209a8c3a5c96c0fa1871cd59
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2018-03-02 18:20:36 +0100

    RHEL: Added best-effort logging to Kickstart post installatoin script

--- AutomatedLabUnattended/Private/RedHat/Export-UnattendedKickstartFile.ps1
+++ AutomatedLabUnattended/Private/RedHat/Export-UnattendedKickstartFile.ps1
@@ -15,11 +15,15 @@ function Export-UnattendedKickstartFile
         fi
       }
     curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+    yum install -y openssl
     yum install -y powershell
     yum install -y omi-psrp-server
 
     if IsNotInstalled powershell; then yum install -y powershell; fi
     if IsNotInstalled omi-psrp-server; then yum install -y powershell; fi
+
+    yum list installed "powershell" > /tmp/ksPowerShell
+    yum list installed "omi-psrp-server" > /tmp/ksOmi
     %end
 '@
     ($script:un | Out-String) -replace "`r`n","`n" | Set-Content -Path $Path -Force
