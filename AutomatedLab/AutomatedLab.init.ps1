d32325636f55d39bd1d3a11eff42039a17e70eae
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2018-03-29 10:45:38 +0200

    LabDrive: Added initialization do init script

--- AutomatedLab/AutomatedLab.init.ps1
+++ AutomatedLab/AutomatedLab.init.ps1
@@ -1,1 +1,7 @@
 Add-Type -Path $PSScriptRoot\AutomatedLab.dll
+
+if (Get-Module -ListAvailable Ships -and (Get-MOdule -ListAvailable AutomatedLab.Ships))
+{
+    Import-Module Ships,AutomatedLab.Ships
+    New-PSDrive -PSProvider SHiPS -Name Labs -Root "AutomatedLab.Ships#LabHost" -WarningAction SilentlyContinue
+}
