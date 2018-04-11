e0cd1ea9ead185d82dc36636c8c0789ef6b64932
Raimund Andée [MSFT] <raandree@live.com>
2018-04-11 15:10:13 +0200

    Adding OS names for eval versions. The old name is the same as for non-eval versions.

--- LabXml/Machines/OperatingSystem.cs
+++ LabXml/Machines/OperatingSystem.cs
@@ -273,6 +273,16 @@ namespace AutomatedLab
                     case "Windows Server 2016 Datacenter (Desktop Experience)":
                         return "CB7KF-BWN84-R7R2Y-793K2-8XDDG";
 
+                    case "Windows Server 2016 Evaluation Standard":
+                        return "WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY";
+                    case "Windows Server 2016 Standard Evaluation (Desktop Experience)":
+                        return "WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY";
+
+                    case "Windows Server 2016 Datacenter Evaluation":
+                        return "CB7KF-BWN84-R7R2Y-793K2-8XDDG";
+                    case "Windows Server 2016 Datacenter Evaluation (Desktop Experience)":
+                        return "CB7KF-BWN84-R7R2Y-793K2-8XDDG";
+
                     //Windows Server 2016 old names
                     case "Windows Server 2016 SERVERSTANDARDCORE":
                         return "WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY";
