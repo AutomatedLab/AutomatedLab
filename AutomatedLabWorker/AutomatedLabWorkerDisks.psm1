6038c3df61b6594b2b8728e04852fb1b591cdefa
Raimund Andée [MSFT] <r.andree@live.com>
2018-04-21 19:18:44 +0200

    Cleaning up the incomplete base image if creation has failed.

--- AutomatedLabWorker/AutomatedLabWorkerDisks.psm1
+++ AutomatedLabWorker/AutomatedLabWorkerDisks.psm1
@@ -34,140 +34,151 @@ function New-LWReferenceVHDX
     $start = Get-Date
     Write-Verbose "Beginning at $start"
     
-    $imageList = Get-LabAvailableOperatingSystem -Path $IsoOsPath
-    Write-Verbose "The Windows Image list contains $($imageList.Count) items"
-	
-    Write-Verbose "Mounting ISO image '$IsoOsPath'"
-    Mount-DiskImage -ImagePath $IsoOsPath
-	
-    Write-Verbose 'Getting disk image of the ISO'
-    $isoImage = Get-DiskImage -ImagePath $IsoOsPath | Get-Volume
-    Write-Verbose "Got disk image '$($isoImage.DriveLetter)'"
-	
-    $isoDrive = "$($isoImage.DriveLetter):"
-    Write-Verbose "OS ISO mounted on drive letter '$isoDrive'"
-	
-    $image = $imageList | Where-Object OperatingSystemName -eq $OsName
-	
-    if (-not $image)
+    try
     {
-        throw "The specified image ('$OsName') could not be found on the ISO '$(Split-Path -Path $IsoOsPath -Leaf)'. Please specify one of the following values: $($imageList.ImageName -join ', ')"
-    }
+        $imageList = Get-LabAvailableOperatingSystem -Path $IsoOsPath
+        Write-Verbose "The Windows Image list contains $($imageList.Count) items"
 	
-    $imageIndex = $image.ImageIndex
-    Write-Verbose "Selected image index '$imageIndex' with name '$($image.ImageName)'"
+        Write-Verbose "Mounting ISO image '$IsoOsPath'"
+        Mount-DiskImage -ImagePath $IsoOsPath
 	
-    $vmDisk = New-VHD -Path $ReferenceVhdxPath -SizeBytes ($SizeInGB * 1GB) -ErrorAction Stop
-    Write-Verbose "Created VHDX file '$($vmDisk.Path)'"
+        Write-Verbose 'Getting disk image of the ISO'
+        $isoImage = Get-DiskImage -ImagePath $IsoOsPath | Get-Volume
+        Write-Verbose "Got disk image '$($isoImage.DriveLetter)'"
 	
-    Write-ScreenInfo -Message "Creating base image for operating system '$OsName'" -NoNewLine -TaskStart
+        $isoDrive = "$($isoImage.DriveLetter):"
+        Write-Verbose "OS ISO mounted on drive letter '$isoDrive'"
+	
+        $image = $imageList | Where-Object OperatingSystemName -eq $OsName
+	
+        if (-not $image)
+        {
+            throw "The specified image ('$OsName') could not be found on the ISO '$(Split-Path -Path $IsoOsPath -Leaf)'. Please specify one of the following values: $($imageList.ImageName -join ', ')"
+        }
+	
+        $imageIndex = $image.ImageIndex
+        Write-Verbose "Selected image index '$imageIndex' with name '$($image.ImageName)'"
+	
+        $vmDisk = New-VHD -Path $ReferenceVhdxPath -SizeBytes ($SizeInGB * 1GB) -ErrorAction Stop
+        Write-Verbose "Created VHDX file '$($vmDisk.Path)'"
+	
+        Write-ScreenInfo -Message "Creating base image for operating system '$OsName'" -NoNewLine -TaskStart
     
-    Mount-DiskImage -ImagePath $ReferenceVhdxPath
-    $vhdDisk = Get-DiskImage -ImagePath $ReferenceVhdxPath | Get-Disk
-    $vhdDiskNumber = [string]$vhdDisk.Number
-    Write-Verbose "Reference image is on disk number '$vhdDiskNumber'"
+        Mount-DiskImage -ImagePath $ReferenceVhdxPath
+        $vhdDisk = Get-DiskImage -ImagePath $ReferenceVhdxPath | Get-Disk
+        $vhdDiskNumber = [string]$vhdDisk.Number
+        Write-Verbose "Reference image is on disk number '$vhdDiskNumber'"
 	
-    Initialize-Disk -Number $vhdDiskNumber -PartitionStyle $PartitionStyle | Out-Null
-    if ($PartitionStyle -eq 'MBR')
-    {
-        $vhdWindowsDrive = New-Partition -DiskNumber $vhdDiskNumber -UseMaximumSize -IsActive -AssignDriveLetter |
-        Format-Volume -FileSystem NTFS -NewFileSystemLabel 'System' -Confirm:$false
-    }
-    else
-    {
-        $vhdRecoveryPartition = New-Partition -DiskNumber $vhdDiskNumber -GptType '{de94bba4-06d1-4d40-a16a-bfd50179d6ac}' -Size 300MB
-        $vhdRecoveryDrive = $vhdRecoveryPartition | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Windows RE Tools' -Confirm:$false
+        Initialize-Disk -Number $vhdDiskNumber -PartitionStyle $PartitionStyle | Out-Null
+        if ($PartitionStyle -eq 'MBR')
+        {
+            $vhdWindowsDrive = New-Partition -DiskNumber $vhdDiskNumber -UseMaximumSize -IsActive -AssignDriveLetter |
+            Format-Volume -FileSystem NTFS -NewFileSystemLabel 'System' -Confirm:$false
+        }
+        else
+        {
+            $vhdRecoveryPartition = New-Partition -DiskNumber $vhdDiskNumber -GptType '{de94bba4-06d1-4d40-a16a-bfd50179d6ac}' -Size 300MB
+            $vhdRecoveryDrive = $vhdRecoveryPartition | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Windows RE Tools' -Confirm:$false
 		
-        $recoveryPartitionNumber = (Get-Disk -Number $vhdDiskNumber | Get-Partition | Where-Object Type -eq Recovery).PartitionNumber
-        $diskpartCmd = @"
+            $recoveryPartitionNumber = (Get-Disk -Number $vhdDiskNumber | Get-Partition | Where-Object Type -eq Recovery).PartitionNumber
+            $diskpartCmd = @"
 select disk $vhdDiskNumber
 select partition $recoveryPartitionNumber
 gpt attributes=0x8000000000000001
 exit
 "@
-        $diskpartCmd | diskpart.exe | Out-Null
+            $diskpartCmd | diskpart.exe | Out-Null
 		
-        $systemPartition = New-Partition -DiskNumber $vhdDiskNumber -GptType '{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}' -Size 100MB
-        #does not work, seems to be a bug. Using diskpart as a workaround
-        #$systemPartition | Format-Volume -FileSystem FAT32 -NewFileSystemLabel 'System' -Confirm:$false
+            $systemPartition = New-Partition -DiskNumber $vhdDiskNumber -GptType '{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}' -Size 100MB
+            #does not work, seems to be a bug. Using diskpart as a workaround
+            #$systemPartition | Format-Volume -FileSystem FAT32 -NewFileSystemLabel 'System' -Confirm:$false
 		
-        $diskpartCmd = "@
-            select disk $vhdDiskNumber
-            select partition $($systemPartition.PartitionNumber)
-            format quick fs=fat32 label=System
-            exit
-        @"
-        $diskpartCmd | diskpart.exe | Out-Null		
+            $diskpartCmd = "@
+                select disk $vhdDiskNumber
+                select partition $($systemPartition.PartitionNumber)
+                format quick fs=fat32 label=System
+                exit
+            @"
+            $diskpartCmd | diskpart.exe | Out-Null		
 		
-        $reservedPartition = New-Partition -DiskNumber $vhdDiskNumber -GptType '{e3c9e316-0b5c-4db8-817d-f92df00215ae}' -Size 128MB
+            $reservedPartition = New-Partition -DiskNumber $vhdDiskNumber -GptType '{e3c9e316-0b5c-4db8-817d-f92df00215ae}' -Size 128MB
 		
-        $vhdWindowsDrive = New-Partition -DiskNumber $vhdDiskNumber -UseMaximumSize -AssignDriveLetter |
-        Format-Volume -FileSystem NTFS -NewFileSystemLabel 'System' -Confirm:$false
-    }
+            $vhdWindowsDrive = New-Partition -DiskNumber $vhdDiskNumber -UseMaximumSize -AssignDriveLetter |
+            Format-Volume -FileSystem NTFS -NewFileSystemLabel 'System' -Confirm:$false
+        }
 	
-    $vhdWindowsVolume = "$($vhdWindowsDrive.DriveLetter):"
-    Write-Verbose "VHD drive '$vhdWindowsDrive', Vhd volume '$vhdWindowsVolume'"
+        $vhdWindowsVolume = "$($vhdWindowsDrive.DriveLetter):"
+        Write-Verbose "VHD drive '$vhdWindowsDrive', Vhd volume '$vhdWindowsVolume'"
 
-    Write-Verbose "Disabling Bitlocker Drive Encryption on drive $vhdWindowsVolume"
-    if (Test-Path -Path C:\Windows\System32\manage-bde.exe)
-    {
-        manage-bde.exe -off $vhdWindowsVolume | Out-Null #without this on some devices (for exmaple Surface 3) the VHD was auto-encrypted
-    }
+        Write-Verbose "Disabling Bitlocker Drive Encryption on drive $vhdWindowsVolume"
+        if (Test-Path -Path C:\Windows\System32\manage-bde.exe)
+        {
+            manage-bde.exe -off $vhdWindowsVolume | Out-Null #without this on some devices (for exmaple Surface 3) the VHD was auto-encrypted
+        }
 	
-    Write-Verbose 'Applying image to the volume...'
+        Write-Verbose 'Applying image to the volume...'
     
-    $wimPath = "$isoDrive\Sources\install.wim"
-    $job = Start-Job -ScriptBlock { 
-        $output = Dism.exe /apply-Image /ImageFile:$using:wimPath /index:$using:imageIndex /ApplyDir:$using:vhdWindowsVolume\
-        New-Object PSObject -Property @{
-            Outout = $output
-            LastExitCode = $LASTEXITCODE
+        $wimPath = "$isoDrive\Sources\install.wim"
+        $job = Start-Job -ScriptBlock { 
+            $output = Dism.exe /apply-Image /ImageFile:$using:wimPath /index:$using:imageIndex /ApplyDir:$using:vhdWindowsVolume\
+            New-Object PSObject -Property @{
+                Outout = $output
+                LastExitCode = $LASTEXITCODE
+            }
+        }
+	
+        $dismResult = Wait-LWLabJob -Job $job -NoDisplay -ProgressIndicator 20 -Timeout 60 -PassThru
+        if ($dismResult.LastExitCode)
+        {
+            throw (New-Object System.ComponentModel.Win32Exception($dismResult.LastExitCode,
+            "The base image for operating system '$OsName' could not be created. The error is $($dismResult.LastExitCode)"))
+        }
+        Start-Sleep -Seconds 5
+    
+        Write-Verbose 'Setting BCDBoot'
+        if ($PartitionStyle -eq 'MBR')
+        {
+            bcdboot.exe $vhdWindowsVolume\Windows /s $vhdWindowsVolume /f BIOS | Out-Null
+        }
+        else
+        {
+            $possibleDrives = [char[]](65..90)
+            $drives = (Get-PSDrive -PSProvider FileSystem).Name
+            $freeDrives = Compare-Object -ReferenceObject $possibleDrives -DifferenceObject $drives | Where-Object { $_.SideIndicator -eq '<=' } 
+            $freeDrive = ($freeDrives | Select-Object -First 1).InputObject
+
+            $diskpartCmd = "@
+                select disk $vhdDiskNumber
+                select partition $($systemPartition.PartitionNumber)
+                assign letter=$freeDrive
+                exit
+            @"
+            $diskpartCmd | diskpart.exe | Out-Null
+		
+            bcdboot.exe $vhdWindowsVolume\Windows /s "$($freeDrive):" /f UEFI | Out-Null
+		
+            $diskpartCmd = "@
+                select disk $vhdDiskNumber
+                select partition $($systemPartition.PartitionNumber)
+                remove letter=$freeDrive
+                exit
+            @"
+            $diskpartCmd | diskpart.exe | Out-Null
         }
     }
-	
-    $dismResult = Wait-LWLabJob -Job $job -NoDisplay -ProgressIndicator 20 -Timeout 60 -PassThru
-    if ($dismResult.LastExitCode)
+    catch
     {
-        throw (New-Object System.ComponentModel.Win32Exception($dismResult.LastExitCode,
-        "The base image for operating system '$OsName' could not be created. The error is $($dismResult.LastExitCode)"))
+        Write-Verbose 'Dismounting ISO and new disk'        
+        Dismount-DiskImage -ImagePath $ReferenceVhdxPath
+        Dismount-DiskImage -ImagePath $IsoOsPath
+        Remove-Item -Path $ReferenceVhdxPath -Force #removing as the creation did not succeed
+        
+        throw $_.Exception
     }
-    Start-Sleep -Seconds 5
     
-    Write-Verbose 'Setting BCDBoot'
-    if ($PartitionStyle -eq 'MBR')
-    {
-        bcdboot.exe $vhdWindowsVolume\Windows /s $vhdWindowsVolume /f BIOS | Out-Null
-    }
-    else
-    {
-        $possibleDrives = [char[]](65..90)
-        $drives = (Get-PSDrive -PSProvider FileSystem).Name
-        $freeDrives = Compare-Object -ReferenceObject $possibleDrives -DifferenceObject $drives | Where-Object { $_.SideIndicator -eq '<=' } 
-        $freeDrive = ($freeDrives | Select-Object -First 1).InputObject
-
-        $diskpartCmd = "@
-            select disk $vhdDiskNumber
-            select partition $($systemPartition.PartitionNumber)
-            assign letter=$freeDrive
-            exit
-        @"
-        $diskpartCmd | diskpart.exe | Out-Null
-		
-        bcdboot.exe $vhdWindowsVolume\Windows /s "$($freeDrive):" /f UEFI | Out-Null
-		
-        $diskpartCmd = "@
-            select disk $vhdDiskNumber
-            select partition $($systemPartition.PartitionNumber)
-            remove letter=$freeDrive
-            exit
-        @"
-        $diskpartCmd | diskpart.exe | Out-Null
-    }
-
     Write-Verbose 'Dismounting ISO and new disk'
-    Dismount-DiskImage -ImagePath $IsoOsPath
     Dismount-DiskImage -ImagePath $ReferenceVhdxPath
-	
+    Dismount-DiskImage -ImagePath $IsoOsPath
     Write-ScreenInfo -Message 'Finished creating base image' -TaskEnd
 	
     $end = Get-Date
@@ -190,7 +201,7 @@ function New-LWVHDX
         [Parameter(Mandatory = $true)]
         [int]$SizeInGB,
 
-		[switch]$SkipInitialize
+        [switch]$SkipInitialize
     )
 	
     Write-LogFunctionEntry
@@ -198,12 +209,12 @@ function New-LWVHDX
     $VmDisk = New-VHD -Path $VhdxPath -SizeBytes ($SizeInGB * 1GB) -ErrorAction Stop
     Write-Verbose "Created VHDX file '$($vmDisk.Path)'"
 	
-	if ($SkipInitialize)
-	{
-		Write-Verbose -Message "Skipping the initialization of '$($vmDisk.Path)'"
-		Write-LogFunctionExit
-		return
-	}
+    if ($SkipInitialize)
+    {
+        Write-Verbose -Message "Skipping the initialization of '$($vmDisk.Path)'"
+        Write-LogFunctionExit
+        return
+    }
 
     $mountedVhd = $VmDisk | Mount-VHD -PassThru
 	
