8302bfcb1c43072d80a495936a6526eefc0c8f02
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2018-09-27 20:10:18 +0300

    Enclosed PW in '' to ensure all possible special chars are taken care of

--- AutomatedLabUnattended/Private/RedHat/Set-UnattendedKickstartAdministratorPassword.ps1
+++ AutomatedLabUnattended/Private/RedHat/Set-UnattendedKickstartAdministratorPassword.ps1
@@ -5,6 +5,6 @@ function Set-UnattendedKickstartAdministratorPassword
 		[string]$Password
     )
 		
-		$Script:un.Add("rootpw $Password")
+		$Script:un.Add("rootpw '$Password'")
 		$Script:un = [System.Collections.Generic.List[string]]($Script:un.Replace('%PASSWORD%', $Password))
 }
