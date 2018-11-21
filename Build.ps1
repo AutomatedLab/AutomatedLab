52f02403a4ff4a12f41d491d46d619b1d791ab74
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2018-11-21 20:37:13 +0100

    Added additional modules to build dependencies as well

--- Build.ps1
+++ Build.ps1
@@ -48,7 +48,7 @@
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Resolve-Module -Name Psake, PSDeploy, Pester, BuildHelpers, AutomatedLab, Ships
+Resolve-Module -Name Psake, PSDeploy, Pester, BuildHelpers, AutomatedLab, Ships, powershell-yaml, newtonsoft.json, Datum
 
 $lastestVersion = Get-Module -Name PackageManagement -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
 if (-not ($lastestVersion.Version -ge '1.1.7.0'))
