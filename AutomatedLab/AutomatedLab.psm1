45642b532222e7fd7c7798774569816752a0c8ac
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-02-14 15:57:35 -0800

    Added Get-LabCache cmdlet

--- AutomatedLab/AutomatedLab.psm1
+++ AutomatedLab/AutomatedLab.psm1
@@ -3829,6 +3829,27 @@ function Clear-LabCache
 }
 #endregion Clear-LabCache
 
+#region Get-LabCache
+function Get-LabCache
+{
+    [CmdletBinding()]
+    param
+    ( )
+
+    $regKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey('CurrentUser', 'Default')
+    $key = $regKey.OpenSubKey('Software\AutomatedLab\Cache')
+    foreach ($value in $key.GetValueNames())
+    {
+        $content = [xml]$key.GetValue($value)
+        $timestamp = $content.SelectSingleNode('//Timestamp')
+        [pscustomobject]@{
+            Store = $value
+            Timestamp = $timestamp.datetime -as [datetime]
+        }
+    }
+}
+#endregion
+
 #region function Add-LabVMUserRight
 function Add-LabVMUserRight
 {
