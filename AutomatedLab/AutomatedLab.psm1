26a4dd0785daddf852e242c3238b0bc479c20640
Raimund Andrée [MSFT] <r.andree@live.com>
2019-02-14 17:51:39 -0800

    Moving code from init file to main module

--- AutomatedLab/AutomatedLab.psm1
+++ AutomatedLab/AutomatedLab.psm1
@@ -4110,3 +4110,113 @@ DatumStructure:
     $settings
 }
 #endregion Get-LabConfigurationItem
+
+#Initialization code
+
+#Register the $LabSources variable
+$dynamicLabSources = New-Object AutomatedLab.DynamicVariable 'global:labSources', { Get-LabSourcesLocationInternal }, { $null }
+$executioncontext.SessionState.PSVariable.Set($dynamicLabSources)
+
+#Import available operating systms from cache (if available)
+Get-LabAvailableOperatingSystem -Path $labSources\ISOs -NoDisplay | Out-Null
+
+#download the ProductKeys.xml file if it does not exist. The installer puts the file into 'C:\ProgramData\AutomatedLab\Assets'
+#but when installing AL using the PowerShell Gallery, this file is missing.
+$productKeyFileLink = 'https://raw.githubusercontent.com/AutomatedLab/AutomatedLab/master/Assets/ProductKeys.xml'
+$productKeyFileName = 'ProductKeys.xml'
+$productKeyFilePath = Join-Path -Path C:\ProgramData\AutomatedLab\Assets -ChildPath $productKeyFileName
+
+if (-not (Test-Path -Path 'C:\ProgramData\AutomatedLab\Assets'))
+{
+    New-Item -Path C:\ProgramData\AutomatedLab\Assets -ItemType Directory | Out-Null
+}
+
+if (-not (Test-Path -Path $productKeyFilePath))
+{
+    Get-LabInternetFile -Uri $productKeyFileLink -Path $productKeyFilePath
+}
+
+$productKeyCustomFileName = 'ProductKeysCustom.xml'
+$productKeyCustomFilePath = Join-Path -Path C:\ProgramData\AutomatedLab\Assets -ChildPath $productKeyCustomFileName
+
+if (-not (Test-Path -Path $productKeyCustomFilePath))
+{
+    $store = New-Object 'AutomatedLab.ListXmlStore[AutomatedLab.ProductKey]'
+    
+    $dummyProductKey = New-Object AutomatedLab.ProductKey -Property @{ Key = '123'; OperatingSystemName = 'OS'; Version = '1.0' }
+    $store.Add($dummyProductKey)
+    $store.Export($productKeyCustomFilePath)
+}
+
+
+$callingCallFrame = (Get-PSCallStack)[1]
+if ($callingCallFrame.ScriptName -like '*AutomatedLab.init.ps1*' -or
+$callingCallFrame.ScriptName -like '*AutomatedLab.psm1*')
+{
+    return
+}
+
+Register-ArgumentCompleter -CommandName Add-LabMachineDefinition -ParameterName OperatingSystem -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    Get-LabAvailableOperatingSystem -Path $labSources\ISOs -UseOnlyCache |
+    Where-Object { ($_.ProductKey -or $_.OperatingSystemType -eq 'Linux') -and $_.OperatingSystemName -like "*$wordToComplete*" } |
+    Group-Object -Property OperatingSystemName |
+    ForEach-Object { $_.Group | Sort-Object -Property Version -Descending | Select-Object -First 1 } |
+    Sort-Object -Property OperatingSystemName |
+    ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new("'$($_.OperatingSystemName)'", "'$($_.OperatingSystemName)'", 'ParameterValue', "$($_.Version) $($_.OperatingSystemName)")
+    }
+}
+
+Register-ArgumentCompleter -CommandName Import-Lab, Remove-Lab -ParameterName Name -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    $path = "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonApplicationData))\AutomatedLab\Labs"
+    Get-ChildItem -Path $path -Directory |
+    ForEach-Object {
+        if ($_.Name -contains ' ')
+        {
+            [System.Management.Automation.CompletionResult]::new("'$($_.Name)'", "'$($_.Name)'", 'ParameterValue', $_.Name)
+        }
+        else
+        {
+            [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+        }
+    }
+}
+
+$commands = Get-Command -Module AutomatedLab*, PSFileTransfer | Where-Object { $_.Parameters.ContainsKey('ComputerName') }
+Register-ArgumentCompleter -CommandName $commands -ParameterName ComputerName -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    Get-LabVM -All -IncludeLinux |
+    ForEach-Object {
+        if ($_.Roles)
+        {
+            [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Roles)
+        }
+        else
+        {
+            [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+        }
+    }
+}
+
+Register-ArgumentCompleter -CommandName Add-LabMachineDefinition -ParameterName DomainName -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    (Get-LabDefinition).Domains |
+    ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+    }
+}
+
+Register-ArgumentCompleter -CommandName Add-LabMachineDefinition -ParameterName Roles -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    [System.Enum]::GetNames([AutomatedLab.Roles]) |
+    ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
