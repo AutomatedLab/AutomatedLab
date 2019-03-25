edf76287050044e69b4bc5199060af0a3da94304
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-03-25 14:28:35 +0100

    Added new Hyper-V role and cmdlet

---
+++ AutomatedLab/AutomatedLabHyperV.psm1
@@ -0,0 +1,53 @@
+function Install-LabHyperV
+{
+    [CmdletBinding()]
+    param
+    ( )
+
+    Write-LogFunctionEntry
+
+    $vms = Get-LabVm -Role HyperV
+
+    Write-ScreenInfo -Message 'Exposing virtualization extensions...' -NoNewLine
+    $hyperVVms = $vms | Where-Object -Property HostType -eq HyperV
+    $hyperVVms | Stop-LabVm -Wait
+    $hyperVVms | Set-VMProcessor -ExposeVirtualizationExtensions $true
+    $vms | Start-LabVm -Wait # Start all, regardless of Hypervisor
+    Write-ScreenInfo -Message 'Done'
+
+    # Enable Feature
+    Write-ScreenInfo -Message "Enabling Hyper-V feature and waiting for restart of $($vms.Count) VMs..." -NoNewLine
+    Install-LabWindowsFeature -ComputerName $vms -FeatureName Hyper-V -IncludeAllSubFeature -IncludeManagementTools -NoDisplay
+
+    # Restart
+    Restart-LabVm -ComputerName $vms -Wait -NoDisplay
+    Write-ScreenInfo -Message 'Done'
+
+    #Configure
+    $settingsTable = @{ }
+
+    foreach ($vm in $vms)
+    {
+        [hashtable]$roleParameters = ($vm.Roles | Where-Object Name -eq HyperV).Properties
+        if ($roleParameters.Count -eq 0) { continue }
+
+        $parameters = Sync-Parameter -Command (Get-Command Set-VMHost) -Parameters $roleParameters
+        $settingsTable.Add($vm.Name, $parameters)
+    }
+
+    if ($settingsTable.Keys.Count -eq 0)
+    {
+        return
+    }
+    
+    Invoke-LabCommand -ActivityName 'Configuring VM Host settings' -ComputerName $settingsTable.Keys -Variable (Get-Variable -Name settingsTable) -ScriptBlock {
+        $vmParameters = $settingsTable[$env:COMPUTERNAME]
+
+        if ($vmParameters)
+        {
+            Set-VMHost @vmParameters
+        }
+    }
+
+    Write-LogFunctionExit
+}
