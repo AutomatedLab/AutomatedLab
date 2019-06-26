324e9363e96a654711550ca2f4f27c8f21a4c3a4
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-06-26 23:12:04 +0200

    count updated

--- AutomatedLab/AutomatedLab.psm1
+++ AutomatedLab/AutomatedLab.psm1
@@ -2511,13 +2511,13 @@ function Install-LabSoftwarePackage
     $fullChild = @($childPath) -eq 'full'
     $libLocation = Split-Path -Parent -Path (Split-Path -Path ([AutomatedLab.Common.Win32Exception]).Assembly.Location -Parent)
     
-    if ($coreChild -and (Invoke-LabCommand -ComputerName $coreChild.ComputerName -NoDisplay -PassThru {Get-Item '/ALLibraries/AutomatedLab.Common.dll' -ErrorAction SilentlyContinue}).Count -ne $coreChild.Count)
+    if ($coreChild -and @(Invoke-LabCommand -ComputerName $coreChild.ComputerName -NoDisplay -PassThru {Get-Item '/ALLibraries/AutomatedLab.Common.dll' -ErrorAction SilentlyContinue}).Count -ne $coreChild.Count)
     {
         $coreLibraryFolder = Join-Path -Path $libLocation -ChildPath $coreChild[0]
         Copy-LabFileItem -Path "$coreLibraryFolder\*.*" -ComputerName $coreChild.ComputerName -DestinationFolderPath '/ALLibraries'
     }
 
-    if ($fullChild -and (Invoke-LabCommand -ComputerName $fullChild.ComputerName -NoDisplay -PassThru {Get-Item '/ALLibraries/AutomatedLab.Common.dll' -ErrorAction SilentlyContinue}).Count -ne $fullChild.Count)
+    if ($fullChild -and @(Invoke-LabCommand -ComputerName $fullChild.ComputerName -NoDisplay -PassThru {Get-Item '/ALLibraries/AutomatedLab.Common.dll' -ErrorAction SilentlyContinue}).Count -ne $fullChild.Count)
     {
         $fullLibraryFolder = Join-Path -Path $libLocation -ChildPath $fullChild[0]
         Copy-LabFileItem -Path "$fullLibraryFolder\*.*" -ComputerName $fullChild.ComputerName -DestinationFolderPath '/ALLibraries'
