1e3a81313896758cabe754c1c4c9ce0acfefc679
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-09-28 12:10:39 +0200

    ALCommon removed entirely from prebuild It is an external module for all intents and purposes

--- Installer/.Build/PreBuild.ps1
+++ Installer/.Build/PreBuild.ps1
@@ -11,16 +11,6 @@
     $InternalModules = @('AutomatedLab','AutomatedLab.Recipe', 'AutomatedLab.Ships','AutomatedLabDefinition','AutomatedLabNotifications','AutomatedLabTest','AutomatedLabUnattended','AutomatedLabWorker','HostsFile','PSFileTransfer','PSLog')
 )
 
-Push-Location
-
-Set-Location -Path $SolutionDir\AutomatedLab.Common
-git reset --hard
-git submodule -q update --init --recursive
-git pull origin master
-
-# Compile Common libary
-dotnet build $SolutionDir\AutomatedLab.Common
-
 Write-Host "Init task - compiling help for Installer"
 if (-not (Get-Module -List PlatyPs))
 {
@@ -29,8 +19,6 @@ if (-not (Get-Module -List PlatyPs))
     Write-Host 'Installing Module PlatyPS'
     Install-Module PlatyPS -Force -AllowClobber -SkipPublisherCheck
 }
-
-$null = New-ExternalHelp -Path $SolutionDir\AutomatedLab.Common\Help\en-us -OutputPath $SolutionDir\AutomatedLab.Common\AutomatedLab.Common\en-us -Force
 
 foreach ($moduleName in (Get-ChildItem -Path $SolutionDir\Help -Directory))
 {
@@ -46,8 +34,6 @@ foreach ($moduleName in (Get-ChildItem -Path $SolutionDir\Help -Directory))
     }
 }
 
-Microsoft.PowerShell.Utility\Write-Host 'Creating backup of file AutomatedLab.Common.psd1'
-Copy-Item -Path $SolutionDir\AutomatedLab.Common\AutomatedLab.Common\AutomatedLab.Common.psd1 -Destination $SolutionDir\AutomatedLab.Common\AutomatedLab.Common\AutomatedLab.Common.psd1.original
 Microsoft.PowerShell.Utility\Write-Host 'Creating backup of file Includes.wxi'
 Copy-Item -Path $SolutionDir\Installer\Includes.wxi -Destination $SolutionDir\Installer\Includes.wxi.original
 Copy-Item -Path $SolutionDir\Installer\Product.wxs -Destination $SolutionDir\Installer\Product.wxs.original
@@ -58,15 +44,8 @@ Microsoft.PowerShell.Utility\Write-Host "AutomatedLab Dll path is '$($automatedL
 $alDllVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($automatedLabdll)
 Microsoft.PowerShell.Utility\Write-Host "Product Version of AutomatedLab is '$($alDllVersion.FileVersion)'"
 
-$alCommonVersion = (Find-Module -Name AutomatedLab.Common -ErrorAction SilentlyContinue).Version
-
-Microsoft.PowerShell.Utility\Write-Host "Version of AutomatedLab.Common is '$alCommonVersion'"
 Microsoft.PowerShell.Utility\Write-Host "Writing new 'Includes.wxi' file"
 ('<?xml version="1.0" encoding="utf-8"?><Include Id="VersionNumberInclude"><?define AutomatedLabCommonVersion="{0}" ?><?define AutomatedLabProductVersion="{1}" ?></Include>' -f $alCommonVersion, $alDllVersion.FileVersion) | Out-File -FilePath ..\Installer\Includes.wxi -Encoding UTF8
-Microsoft.PowerShell.Utility\Write-Host "Replacing version in 'AutomatedLab.Common.psd1' file"
-(Get-Content -Path .\AutomatedLab.Common\AutomatedLab.Common.psd1 -Raw) -replace "(ModuleVersion([ =]+))(')(?<Version>\d{1,2}\.\d{1,2}\.\d{1,2})", "`$1'$alCommonVersion" | Out-File -FilePath .\AutomatedLab.Common\AutomatedLab.Common.psd1
-
-Pop-Location
 
 Microsoft.PowerShell.Utility\Write-Host "Dynamically adding modules to product.wxs"
 $xmlContent = [xml](Get-Content $SolutionDir\Installer\product.wxs)
