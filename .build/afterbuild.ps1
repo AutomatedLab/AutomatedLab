88037b1552d32315a7ffd94a52c9d3fce0e6e52c
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2020-04-16 14:37:13 +0200

    Additional OS added

--- .build/afterbuild.ps1
+++ .build/afterbuild.ps1
@@ -1,5 +1,5 @@
 ﻿Write-Host "Calling build script"
-./Build.ps1
+& (Join-Path -Path $env:APPVEYOR_BUILD_FOLDER './.build/Build.ps1')
 Write-Host "'after_build' block"
 $Params = @{
     Path    = $env:APPVEYOR_BUILD_FOLDER
@@ -11,9 +11,9 @@ Invoke-PSDeploy @Params # Create nuget package artifacts
 Write-Host "Locating installer to push as artifact"
 
 Add-AppveyorMessage "Locating installer to push as artifact" -Category Information
-$msifile = Get-ChildItem -Recurse -Filter AutomatedLab.msi | Select-Object -First 1
+$msifile = Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse -Filter AutomatedLab.msi | Select-Object -First 1
 Push-AppVeyorArtifact $msifile.FullName -FileName $msifile.Name -DeploymentName alinstaller
 
 Add-AppveyorMessage "Locating debian package to push as artifact" -Category Information
-$debFile = Get-ChildItem -Recurse -Filter automatedlab.deb | Select-Object -First 1
+$debFile = Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse -Filter automatedlab.deb | Select-Object -First 1
 Push-AppVeyorArtifact $debFile.FullName -FileName $debFile.Name -DeploymentName aldebianpackage
