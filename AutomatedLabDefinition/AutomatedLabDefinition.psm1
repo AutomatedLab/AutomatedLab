61953390867ea974d591687531c913d111d607dd
Raimund Andée [MSFT] <raandree@live.com>
2020-04-18 12:44:08 +0200

    Removing 'DisableAntiSpyware' for Windows 10 1909 deployments.

--- AutomatedLabDefinition/AutomatedLabDefinition.psm1
+++ AutomatedLabDefinition/AutomatedLabDefinition.psm1
@@ -13,9 +13,6 @@ $unattendedXmlDefaultContent2012 = @'
     </component>
   </settings>
   <settings pass="specialize">
-    <component name="Security-Malware-Windows-Defender" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <DisableAntiSpyware>true</DisableAntiSpyware>
-    </component>
     <component name="Microsoft-Windows-UnattendedJoin" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State">
       <Identification>
         <JoinWorkgroup xmlns="">NET</JoinWorkgroup>
