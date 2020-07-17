3ff8f9d8449f74d37f5d80466a926cbe47bcfbf7
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2020-07-17 16:08:18 +0200

    Damn licensing

--- AutomatedLab/AutomatedLabScvmm.psm1
+++ AutomatedLab/AutomatedLabScvmm.psm1
@@ -45,8 +45,8 @@ function Install-LabScvmm
     $odbcFile = Get-LabInternetFile -Uri $odbc -Path $labsources\Tools -FileName odbc.msi -PassThru
     $adkFile = Get-LabInternetFile -Uri $adk -Path $labsources\Tools -FileName adk.exe -PassThru
     $adkpeFile = Get-LabInternetFile -Uri $adkpe -Path $labsources\Tools -FileName adkpe.exe -PassThru
-    Install-LabSoftwarePackage -Path $odbcFile.FullName -ComputerName $all -CommandLine '/QN ADDLOCAL=ALL'
-    Install-LabSoftwarePackage -Path $sqlFile.FullName -ComputerName $all
+    Install-LabSoftwarePackage -Path $odbcFile.FullName -ComputerName $all -CommandLine '/QN ADDLOCAL=ALL IACCEPTMSODBCSQLLICENSETERMS=YES /L*v C:\odbc.log'
+    Install-LabSoftwarePackage -Path $sqlFile.FullName -ComputerName $all -CommandLine '/QN IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES/L*v C:\sqlcmd.log'
     
     if ($(Get-Lab).DefaultVirtualizationEngine -eq 'Azure' -or (Test-LabMachineInternetConnectivity -ComputerName $all[0]))
     {
