fdebe683ff690ae9105ad41b408baa997d90b1ac
Raimund Andree <r.andree@live.com>
2021-02-03 18:19:59 +0100

    Added error handling

--- AutomatedLab/AutomatedLabADDS.psm1
+++ AutomatedLab/AutomatedLabADDS.psm1
@@ -2096,8 +2096,7 @@ function New-LabADSite
         }
     }
 
-    $null = Invoke-LabCommand -ComputerName $rootDc -NoDisplay -PassThru -ScriptBlock `
-    {
+    $createSiteCmd = {
         param
         (
             $ComputerName, $SiteName, $SiteSubnet
@@ -2153,7 +2152,19 @@ function New-LabADSite
                 }
             }
         }
-    } -ArgumentList $ComputerName, $SiteName, $SiteSubnet
+    }
+
+    try
+    {
+        $null = Invoke-LabCommand -ComputerName $rootDc -NoDisplay -PassThru -ScriptBlock $createSiteCmd `
+        -ArgumentList $ComputerName, $SiteName, $SiteSubnet -ErrorAction Stop
+    }
+    catch {
+        Restart-LabVM -ComputerName $ComputerName -Wait
+        
+        Invoke-LabCommand -ComputerName $rootDc -NoDisplay -PassThru -ScriptBlock $createSiteCmd `
+        -ArgumentList $ComputerName, $SiteName, $SiteSubnet -ErrorAction Stop
+    }
 
     Write-LogFunctionExit
 }
