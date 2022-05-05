822f8dc198d45f70489abcb3828918300659f8f5
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-05-05 13:14:19 +0200

    Duplicate code removed

--- AutomatedLab/AutomatedLabADDS.psm1
+++ AutomatedLab/AutomatedLabADDS.psm1
@@ -853,33 +853,6 @@ function Install-LabRootDcs
                 'C:\Windows\NTDS'
             }
 
-            $netBiosDomainName = if ($dcRole.Properties.ContainsKey('NetBiosDomainName'))
-            {
-                $dcRole.Properties.NetBiosDomainName
-            }
-            else
-            {
-                $machine.DomainName.Substring(0, $machine.DomainName.IndexOf('.'))
-            }
-
-            $databasePath = if ($dcRole.Properties.ContainsKey('DatabasePath'))
-            {
-                $dcRole.Properties.DatabasePath
-            }
-            else
-            {
-                'C:\Windows\NTDS'
-            }
-
-            $logPath = if ($dcRole.Properties.ContainsKey('LogPath'))
-            {
-                $dcRole.Properties.LogPath
-            }
-            else
-            {
-                'C:\Windows\NTDS'
-            }
-
             $sysvolPath = if ($dcRole.Properties.ContainsKey('SysvolPath'))
             {
                 $dcRole.Properties.SysvolPath
