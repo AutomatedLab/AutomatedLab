9bea2de3af72aaf0cc41999279ac73f3a3e13d43
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-05-06 14:39:16 +0200

    Skipping test for hidden AzureJitTimestamp setting

--- Tests/Integration/Get-LabConfigurationItem.tests.ps1
+++ Tests/Integration/Get-LabConfigurationItem.tests.ps1
@@ -31,7 +31,7 @@
     $functionCalls = (Get-ChildItem -Path "$rootpath\..\.." -Recurse -Filter *.ps*1 | Select-String -Pattern 'Get-LabConfigurationItem -Name [\w\.-]+').Matches.Value | Sort-Object -Unique
     
     # LabSourcesLocation and VmPath are user-defined and just pre-initialized with $null
-    $skippedCalls = @('Sql', 'LabSourcesLocation', 'VmPath')
+    $skippedCalls = @('Sql', 'LabSourcesLocation', 'VmPath', 'AzureJitTimestamp')
     $testCases = foreach ($call in $functionCalls)
     {
         if ($call -notmatch '-Name\s(?<Name>[\w\.-]+)') { continue }
