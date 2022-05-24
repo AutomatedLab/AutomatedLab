e2f696a2db8af0d3b777f787d2807447c629e758
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-05-24 09:48:18 +0200

    Fixing issue with empty default NIC

--- AutomatedLab/AutomatedLab.psm1
+++ AutomatedLab/AutomatedLab.psm1
@@ -868,7 +868,8 @@ function Install-Lab
             foreach ($machine in ($Script:data.Machines | Where-Object {[string]::IsNullOrEmpty($_.FriendlyName)}))
             {
                 if ($machine.HostType -ne 'HyperV' -or (Get-LabConfigurationItem -Name SkipHostFileModification)) { continue }
-                $address = ($machine.NetworkAdapters | Where-Object Default)[0].Ipv4Address
+                $defaultNic = $machine.NetworkAdapters | Where-Object Default
+                $address = if ($defaultNic) {($defaultNic | Select-Object -First 1).Ipv4Address}
                 if (-not $address)
                 {
                     $address = $machine.NetworkAdapters[0].Ipv4Address
