96ff15c6e98f287359e2daeab86081d8ad9a9750
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-07-25 14:53:22 +0200

    Messaging

--- .build/afterbuild.ps1
+++ .build/afterbuild.ps1
@@ -30,11 +30,13 @@ if ($IsLinux)
 # Do not publish artifacts anywhere if not on master or develop
 if ($env:APPVEYOR_REPO_BRANCH -in 'master', 'develop')
 {
+    Add-AppveyorMessage "$env:APPVEYOR_REPO_BRANCH -in 'master', 'develop' - Exiting build" -Category Information
     Exit-AppveyorBuild
 }
 
 # Do not publish artifacts to Gallery or GitHub with every PR
 if ($env:APPVEYOR_REPO_BRANCH -in 'master', 'develop' -and -not [string]::IsNullOrWhitespace($env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH) )
 {
+    Add-AppveyorMessage "$env:APPVEYOR_REPO_BRANCH -in 'master', 'develop', PR coming in from $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH - Exiting build" -Category Information
     Exit-AppveyorBuild
 }
