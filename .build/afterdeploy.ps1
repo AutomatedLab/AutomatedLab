77183db92396294210bca8298c95ebd062d7ab8b
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-07-26 11:14:53 +0200

    Reduce more output

--- .build/afterdeploy.ps1
+++ .build/afterdeploy.ps1
@@ -19,11 +19,25 @@ $sourceLibraryPathCore = Join-Path $env:APPVEYOR_BUILD_FOLDER 'LabXml/bin/Debug/
 
 if (-not (Test-Path $mainModule))
 {
-    robocopy /S /E $sourceLibraryPath (Split-Path $mainModule)
+    $null = robocopy /S /E $sourceLibraryPath (Split-Path $mainModule)
 }
 if (-not (Test-Path $mainModuleCore))
 {
-    robocopy /S /E $sourceLibraryPathCore (Split-Path $mainModuleCore)
+    $null = robocopy /S /E $sourceLibraryPathCore (Split-Path $mainModuleCore)
+}
+
+# Do not publish artifacts anywhere if not on master or develop
+if ($env:APPVEYOR_REPO_BRANCH -notin 'master', 'develop')
+{
+    Add-AppveyorMessage "$env:APPVEYOR_REPO_BRANCH -notin 'master', 'develop' - Exiting build" -Category Information
+    Exit-AppveyorBuild
+}
+
+# Do not publish artifacts to Gallery or GitHub with every PR
+if ($env:APPVEYOR_REPO_BRANCH -in 'master', 'develop' -and -not [string]::IsNullOrWhitespace($env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH) )
+{
+    Add-AppveyorMessage "$env:APPVEYOR_REPO_BRANCH -in 'master', 'develop', PR coming in from $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH - Exiting build" -Category Information
+    Exit-AppveyorBuild
 }
 
 $env:PSModulePath += ";$env:APPVEYOR_BUILD_FOLDER"
