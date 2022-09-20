f60ac9dcd4295850a7fcbd5a81060ffbf9920c0b
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-09-20 13:55:48 +0200

    Ternary operator executed Az cmds in non Az labs

--- Help/Wiki/SampleScripts/Scenarios/en-us/AzureArcConnectedHyperV.md
+++ Help/Wiki/SampleScripts/Scenarios/en-us/AzureArcConnectedHyperV.md
@@ -32,24 +32,29 @@ After the deployment using `Install-Lab` is done, the onboarding steps are execu
 #>
 param
 (
+    # Name of the lab
     [Parameter(ParameterSetName = 'SubId')]
     [Parameter(ParameterSetName = 'SubName')]
     [string]
     $LabName = 'HyperVWithArc',
 
+    # GUID of the subscription to be used
     [Parameter(ParameterSetName = 'SubId', Mandatory = $true)]
     [guid]
     $SubscriptionId,
 
+    # Name of the subscription to be used
     [Parameter(ParameterSetName = 'SubName', Mandatory = $true)]
     [string]
     $SubscriptionName,
 
+    # Name of the resource group Arc enabled machines should be placed in
     [Parameter(ParameterSetName = 'SubId')]
     [Parameter(ParameterSetName = 'SubName')]
     [string]
     $ArcResourceGroupName = 'ALArc',
 
+    # Location of both the Arc resource group as well as the Arc enabled machines
     [Parameter(ParameterSetName = 'SubId')]
     [Parameter(ParameterSetName = 'SubName')]
     [string]
@@ -98,16 +103,24 @@ if (-not $sqlIso)
 
 if (-not (Test-Path $sqlIso)) { throw "SQL ISO $sqlIso not found." }
 
-Add-LabIsoImageDefinition -Name SQLServer2019
+Add-LabIsoImageDefinition -Name SQLServer2019 -Path $sqlIso
+
+Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace 192.168.42.0/24
+Add-LabVirtualNetworkDefinition -Name 'Default Switch' -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet' }
 
 $PSDefaultParameterValues = @{
     'Add-LabMachineDefinition:DomainName' = 'contoso.com'
     'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2022 Datacenter'
     'Add-LabMachineDefinition:Memory' = 2GB
+    'Add-LabMachineDefinition:Network' = $labName
 }
 
 Add-LabMachineDefinition -Name ARCDC01 -Role RootDC
 Add-LabMachineDefinition -Name ARCCA01 -Role CARoot
+$netAdapter = @()
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $labName -Ipv4Address 192.168.42.50
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch 'Default Switch' -UseDhcp
+Add-LabMachineDefinition -Name ARCGW01 -Role Routing -NetworkAdapter $netAdapter
 Add-LabMachineDefinition -Name ARCDB01 -OperatingSystem 'Windows Server 2022 Datacenter (Desktop Experience)' -Role SQLServer2019
 Add-LabMachineDefinition -Name ARCWB01 -Role WebServer
 Add-LabMachineDefinition -Name ARCWB02 -Role WebServer
--- LabSources/SampleScripts/Scenarios/AzureArcConnectedHyperV.ps1
+++ LabSources/SampleScripts/Scenarios/AzureArcConnectedHyperV.ps1
@@ -13,24 +13,29 @@
 #>
 param
 (
+    # Name of the lab
     [Parameter(ParameterSetName = 'SubId')]
     [Parameter(ParameterSetName = 'SubName')]
     [string]
     $LabName = 'HyperVWithArc',
 
+    # GUID of the subscription to be used
     [Parameter(ParameterSetName = 'SubId', Mandatory = $true)]
     [guid]
     $SubscriptionId,
 
+    # Name of the subscription to be used
     [Parameter(ParameterSetName = 'SubName', Mandatory = $true)]
     [string]
     $SubscriptionName,
 
+    # Name of the resource group Arc enabled machines should be placed in
     [Parameter(ParameterSetName = 'SubId')]
     [Parameter(ParameterSetName = 'SubName')]
     [string]
     $ArcResourceGroupName = 'ALArc',
 
+    # Location of both the Arc resource group as well as the Arc enabled machines
     [Parameter(ParameterSetName = 'SubId')]
     [Parameter(ParameterSetName = 'SubName')]
     [string]
@@ -81,14 +86,22 @@ if (-not (Test-Path $sqlIso)) { throw "SQL ISO $sqlIso not found." }
 
 Add-LabIsoImageDefinition -Name SQLServer2019 -Path $sqlIso
 
+Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace 192.168.42.0/24
+Add-LabVirtualNetworkDefinition -Name 'Default Switch' -HyperVProperties @{ SwitchType = 'External'; AdapterName = 'Ethernet' }
+
 $PSDefaultParameterValues = @{
     'Add-LabMachineDefinition:DomainName' = 'contoso.com'
     'Add-LabMachineDefinition:OperatingSystem' = 'Windows Server 2022 Datacenter'
     'Add-LabMachineDefinition:Memory' = 2GB
+    'Add-LabMachineDefinition:Network' = $labName
 }
 
 Add-LabMachineDefinition -Name ARCDC01 -Role RootDC
 Add-LabMachineDefinition -Name ARCCA01 -Role CARoot
+$netAdapter = @()
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $labName -Ipv4Address 192.168.42.50
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch 'Default Switch' -UseDhcp
+Add-LabMachineDefinition -Name ARCGW01 -Role Routing -NetworkAdapter $netAdapter
 Add-LabMachineDefinition -Name ARCDB01 -OperatingSystem 'Windows Server 2022 Datacenter (Desktop Experience)' -Role SQLServer2019
 Add-LabMachineDefinition -Name ARCWB01 -Role WebServer
 Add-LabMachineDefinition -Name ARCWB02 -Role WebServer
@@ -99,4 +112,8 @@ Save-Module -Name Az.ConnectedMachine -Repository PSGallery -Path "$labSources/T
 Import-Module -Name "$labSources/Tools/Az.ConnectedMachine"
 $sessions = New-LabPSSession -ComputerName (Get-LabVm)
 Invoke-LabCommand -ComputerName (Get-LabVm) { [Net.ServicePointManager]::SecurityProtocol += [Net.SecurityProtocolType]::Tls12 } # Yes, really...
-Connect-AzConnectedMachine -ResourceGroupName $ArcResourceGroupName -PSSession $sessions -Location $Location
+foreach ($session in $sessions)
+{
+    # Connect-AzConnectedMachine has a severe bug if more than one session is passed. Machines are onboarded, but errors are thrown.
+    Connect-AzConnectedMachine -ResourceGroupName $ArcResourceGroupName -PSSession $session -Location $Location
+}
