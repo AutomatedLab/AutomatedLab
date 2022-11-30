b22e188a08ed9c41a702ae48324f236fceda4ae6
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2022-11-30 16:10:38 +0100

    IntegTests include Azure

--- .build/AlIntegrationEnv.ps1
+++ .build/AlIntegrationEnv.ps1
@@ -1,15 +1,23 @@
 ﻿$null = mkdir D:\AL
 
 $ProgressPreference = 'SilentlyContinue'
-$labname = "Integrator$($PSVersionTable.PSVersion.Major)$($PSVersionTable.PSVersion.Minor)"
-New-LabDefinition -Name $labname -DefaultVirtualizationEngine HyperV -VmPath D:\AL
 
-# These tests should eventually cover EVERY role, so that Invoke-LabPester creates a valid test file
-# They should also cover every combination of Operating Systems
-# One idea would be to create several labs and have them run as jobs, hopefully working as intended
-Add-LabMachineDefinition -Name DC1 -Memory 1GB -OperatingSystem 'Windows Server 2022 Datacenter' -Roles RootDC -DomainName contoso.com
-Add-LabMachineDefinition -Name CL1 -Memory 1GB -OperatingSystem 'Windows Server 2022 Datacenter' -DomainName contoso.com
+foreach ($hypervisor in 'HyperV','Az')
+{
+    $labname = "IntegratorHyperV$($PSVersionTable.PSVersion.Major)$($PSVersionTable.PSVersion.Minor)"
+    New-LabDefinition -Name $labname -DefaultVirtualizationEngine $hypervisor -VmPath D:\AL
+    if ($hypervisor -eq 'Az')
+    {
+        Add-LabAzureSubscription -DefaultLocationName 'West Europe'
+    }
 
-Install-Lab -NoValidation
+    # These tests should eventually cover EVERY role, so that Invoke-LabPester creates a valid test file
+    # They should also cover every combination of Operating Systems
+    # One idea would be to create several labs and have them run as jobs, hopefully working as intended
+    Add-LabMachineDefinition -Name DC1 -Memory 1GB -OperatingSystem 'Windows Server 2022 Datacenter' -Roles RootDC -DomainName contoso.com
+    Add-LabMachineDefinition -Name CL1 -Memory 1GB -OperatingSystem 'Windows Server 2022 Datacenter' -DomainName contoso.com
 
-Invoke-LabPester -OutputFile "C:\TestResult$labname.xml" -LabName $labname -ErrorAction SilentlyContinue
+    Install-Lab -NoValidation
+
+    Invoke-LabPester -OutputFile "C:\TestResult$($hypervisor)$($labname).xml" -LabName $labname -ErrorAction SilentlyContinue
+}
