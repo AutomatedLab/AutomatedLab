148afe449eff256ef0376382dc1dc1733efe1cd2
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-02-09 09:20:35 +0100

    Added SCOM tests

---
+++ AutomatedLabTest/internal/tests/ScomWebConsole.tests.ps1
@@ -0,0 +1,20 @@
+Describe "[$($Lab.Name)] ScomWebConsole" -Tag ScomWebConsole {
+    Context "Role deployment successful" {
+            It "[ScomWebConsole] Should return the correct amount of machines" {
+                (Get-LabVm -Role ScomWebConsole).Count | Should -Be $(Get-Lab).Machines.Where({$_.Roles.Name -contains 'ScomWebConsole'}).Count
+            }
+            
+            foreach ($vm in (Get-LabVM -Role ScomWebConsole))
+            {
+                It "[$vm] Should have SCOM web console installed" -TestCases @{
+                    vm = $vm
+                } {
+                    Invoke-LabCommand -ComputerName $vm -NoDisplay -PassThru -ScriptBlock {
+                        (Get-Package -Name 'System Center Operations Web Console' -Provider msi -ErrorAction SilentlyContinue).Name
+                    } | Should -Be 'System Center Operations Web Console'
+                }
+            }
+        }
+    }
+    
+    
