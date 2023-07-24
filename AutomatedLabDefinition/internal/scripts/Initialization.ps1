e9e5cff5452f12574dea4464bff5fb2505a32ea5
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-07-24 16:21:56 +0200

    hostname, timezone and initial user

--- AutomatedLabDefinition/internal/scripts/Initialization.ps1
+++ AutomatedLabDefinition/internal/scripts/Initialization.ps1
@@ -637,10 +637,15 @@ packages:
   - authselect-compat
   - sshd
   - powershell
-identity:
-  username: {}
-  hostname: {}
-  password: {}
+ssh_pwauth: true
+users:
+  - default
+system_info:
+  default_user: {}
+hostname: {}
+password: {}
+chpasswd:
+    expire: false
 late-commands:
   - 'echo "Subsystem powershell /usr/bin/pwsh -sshs -NoLogo" >> /etc/ssh/sshd_config'
 '@
