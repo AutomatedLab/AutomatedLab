fe33ea6b18fab8bd7c0c410e744ff58416e067d0
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-07-25 10:47:10 +0200

    Update provider-specific cmds

--- AutomatedLabUnattended/internal/functions/Suse/Add-UnattendedYastSshPublicKey.ps1
+++ AutomatedLabUnattended/internal/functions/Suse/Add-UnattendedYastSshPublicKey.ps1
@@ -8,5 +8,25 @@ function Add-UnattendedYastSshPublicKey
         $PublicKey
     )
 
-    Write-PSFMessage -Message "No unattended ssh key import on Autoyast yet"
+    <#
+    <authorized_keys config:type="list">
+    <listentry>ssh-rsa ...</listentry>
+    </authorized_keys>
+    #>
+    $userNode = $script:un.SelectSingleNode('/un:profile/un:users', $script:nsm)
+    foreach ($user in $userNode.ChildNodes)
+    {
+        if (-not $user.authorized_keys)
+        {
+            $keyNode = $script:un.CreateElement('authorized_keys', $script:nsm.LookupNamespace('un'))
+            $keyNode.SetAttribute('type', $script:nsm.LookupNamespace('config'), 'list')
+            $null = $user.AppendChild($keyNode)
+        }
+
+        $keyNode = $user.authorized_keys
+        if ($keyNode.listentry -contains $PublicKey) { continue }
+        $listEntry = $script:un.CreateElement('listentry', $script:nsm.LookupNamespace('un'))
+        $listEntry.InnerText = $PublicKey
+        $null = $keyNode.AppendChild($listEntry)
+    }
 }
