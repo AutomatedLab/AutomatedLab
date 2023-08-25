2f6cb3ef829cac56d3abaf19624f67a821cc1c45
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-08-25 12:21:03 +0200

    Giving up on repo config

--- AutomatedLabDefinition/internal/scripts/Initialization.ps1
+++ AutomatedLabDefinition/internal/scripts/Initialization.ps1
@@ -675,28 +675,6 @@ autoinstall:
       primary:
         - country-mirror
         - uri: http://archive.ubuntu.com/ubuntu
-    sources:
-      microsoft-powershell.list:
-        key: |
-          -----BEGIN PGP PUBLIC KEY BLOCK-----
-          Version: GnuPG v1.4.7 (GNU/Linux)
-          mQENBFYxWIwBCADAKoZhZlJxGNGWzqV+1OG1xiQeoowKhssGAKvd+buXCGISZJwT
-          LXZqIcIiLP7pqdcZWtE9bSc7yBY2MalDp9Liu0KekywQ6VVX1T72NPf5Ev6x6DLV
-          7aVWsCzUAF+eb7DC9fPuFLEdxmOEYoPjzrQ7cCnSV4JQxAqhU4T6OjbvRazGl3ag
-          OeizPXmRljMtUUttHQZnRhtlzkmwIrUivbfFPD+fEoHJ1+uIdfOzZX8/oKHKLe2j
-          H632kvsNzJFlROVvGLYAk2WRcLu+RjjggixhwiB+Mu/A8Tf4V6b+YppS44q8EvVr
-          M+QvY7LNSOffSO6Slsy9oisGTdfE39nC7pVRABEBAAG0N01pY3Jvc29mdCAoUmVs
-          ZWFzZSBzaWduaW5nKSA8Z3Bnc2VjdXJpdHlAbWljcm9zb2Z0LmNvbT6JATUEEwEC
-          AB8FAlYxWIwCGwMGCwkIBwMCBBUCCAMDFgIBAh4BAheAAAoJEOs+lK2+EinPGpsH
-          /32vKy29Hg51H9dfFJMx0/a/F+5vKeCeVqimvyTM04C+XENNuSbYZ3eRPHGHFLqe
-          MNGxsfb7C7ZxEeW7J/vSzRgHxm7ZvESisUYRFq2sgkJ+HFERNrqfci45bdhmrUsy
-          7SWw9ybxdFOkuQoyKD3tBmiGfONQMlBaOMWdAsic965rvJsd5zYaZZFI1UwTkFXV
-          KJt3bp3Ngn1vEYXwijGTa+FXz6GLHueJwF0I7ug34DgUkAFvAs8Hacr2DRYxL5RJ
-          XdNgj4Jd2/g6T9InmWT0hASljur+dJnzNiNCkbn9KbX7J/qK1IbR8y560yRmFsU+
-          NdCFTW7wY0Fb1fWJ+/KTsC4=
-          =J6gs
-          -----END PGP PUBLIC KEY BLOCK-----
-        source: deb [arch=amd64,armhf,arm64] https://packages.microsoft.com/ubuntu/22.04/prod $RELEASE main
   packages:
     - oddjob
     - oddjob-mkhomedir
@@ -704,9 +682,10 @@ autoinstall:
     - adcli
     - realmd
     - samba-common
-    - authselect-compat
     - ssh
-    - powershell
+    - wget
+    - apt-transport-https
+    - software-properties-common
   user-data:
     system_info:
       default_user: {}
@@ -716,6 +695,10 @@ autoinstall:
     chpasswd:
       expire: false
   late-commands:
+    - wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+    - dpkg -i packages-microsoft-prod.deb
+    - apt update
+    - apt install -y powershell
     - 'echo "Subsystem powershell /usr/bin/pwsh -sshs -NoLogo" >> /etc/ssh/sshd_config'
     - DEBIAN_FRONTEND=noninteractive apt-get -yq install krb5-user
 '@
