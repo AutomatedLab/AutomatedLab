ff10a3dd8851267ffea837f0636b124ed83f6949
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-08-30 14:50:45 +0200

    Try domain join differently

--- AutomatedLabUnattended/internal/functions/CloudInit/Set-UnattendedCloudInitDomain.ps1
+++ AutomatedLabUnattended/internal/functions/CloudInit/Set-UnattendedCloudInitDomain.ps1
@@ -19,15 +19,17 @@
 	{
 		$script:un['autoinstall']['user-data']['fqdn'] = '{0}.{1}' -f $script:un['autoinstall']['user-data']['hostname'].ToLower(), $DomainName
 	}
-
-	$script:un['autoinstall']['late-commands'] += 'curtin in-target --target=/target -- apt install -yq realmd sssd-tools sssd libnss-sss libpam-sss adcli policycoreutils'
-
-	if ($OrganizationalUnit)
-	{
-		$script:un['autoinstall']['late-commands'] += "curtin in-target --target=/target -- echo '{0}' | realm join --computer-ou='{2}' -U {3} {1}" -f $Password, $DomainName, $OrganizationalUnit, $UserName
-	}
-	else
-	{
-		$script:un['autoinstall']['late-commands'] += "curtin in-target --target=/target -- echo '{0}' | realm join -U {2} {1}" -f $Password, $DomainName, $UserName
+	
+	$script:un['autoinstall']['user-data']['write-files'] += @{
+		append  = $false
+		path    = '/etc/cron.d/realmjoin'
+		content = if ($OrganizationalUnit)
+		{
+			"@reboot root echo '{0}' | realm join --computer-ou='{2}' -U {3} {1}`"" -f $Password, $DomainName, $OrganizationalUnit, $UserName
+		}
+		else
+		{
+			"@reboot root echo '{0}' | realm join -U {2} {1}`"" -f $Password, $DomainName, $UserName
+		}
 	}
 }
