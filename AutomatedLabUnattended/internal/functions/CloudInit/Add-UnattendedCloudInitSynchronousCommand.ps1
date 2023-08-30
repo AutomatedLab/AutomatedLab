2100fe86e9c5bd907ce292e2cac3ad877baeac0c
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-08-30 13:11:25 +0200

    Run commands in target env

--- AutomatedLabUnattended/internal/functions/CloudInit/Add-UnattendedCloudInitSynchronousCommand.ps1
+++ AutomatedLabUnattended/internal/functions/CloudInit/Add-UnattendedCloudInitSynchronousCommand.ps1
@@ -10,6 +10,11 @@
     )
 
     # Ensure that installer runs to completion by returning with exit code 0
+    if ($Command -notlike 'curtin in-target --target=/target -*')
+    {
+        $Command = "curtin in-target --target=/target -- $Command"
+    }
+
     $Command = "$Command; exit 0"
     $script:un['autoinstall']['late-commands'] += $Command
 }
--- AutomatedLabUnattended/internal/functions/CloudInit/Set-UnattendedCloudInitDomain.ps1
+++ AutomatedLabUnattended/internal/functions/CloudInit/Set-UnattendedCloudInitDomain.ps1
@@ -20,14 +20,14 @@
 		$script:un['autoinstall']['user-data']['fqdn'] = '{0}.{1}' -f $script:un['autoinstall']['user-data']['hostname'].ToLower(), $DomainName
 	}
 
-	$script:un['autoinstall']['late-commands'] += 'DEBIAN_FRONTEND=noninteractive apt install -yq realmd sssd-tools sssd libnss-sss libpam-sss adcli policycoreutils'
+	$script:un['autoinstall']['late-commands'] += 'curtin in-target --target=/target -- DEBIAN_FRONTEND=noninteractive apt install -yq realmd sssd-tools sssd libnss-sss libpam-sss adcli policycoreutils'
 
 	if ($OrganizationalUnit)
 	{
-		$script:un['autoinstall']['late-commands'] += "echo '{0}' | realm join --computer-ou='{2}' -U {3} {1}" -f $Password, $DomainName, $OrganizationalUnit, $UserName
+		$script:un['autoinstall']['late-commands'] += "curtin in-target --target=/target -- echo '{0}' | realm join --computer-ou='{2}' -U {3} {1}" -f $Password, $DomainName, $OrganizationalUnit, $UserName
 	}
 	else
 	{
-		$script:un['autoinstall']['late-commands'] += "echo '{0}' | realm join -U {2} {1}" -f $Password, $DomainName, $UserName
+		$script:un['autoinstall']['late-commands'] += "curtin in-target --target=/target -- echo '{0}' | realm join -U {2} {1}" -f $Password, $DomainName, $UserName
 	}
 }
