4edfc346077a1f336016004f090cf8b089bb45d4
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-10-10 10:42:14 +0200

    Order cmdlets alphabetically

--- NewOrUpdateHelp.ps1
+++ NewOrUpdateHelp.ps1
@@ -16,11 +16,11 @@
         'AutomatedLab.Recipe')
 )
 
-$location = Join-Path $PSScriptRoot publish -Resolve -ErrorAction Stop
+$buildFolder = if ($env:APPVEYOR_BUILD_FOLDER) { $env:APPVEYOR_BUILD_FOLDER } else { $PSScriptRoot }
 
 $outPath = foreach ($moduleName in $Module)
 {
-    $outputFolder = Join-Path $location -ChildPath "Help/$moduleName/en-us"
+    $outputFolder = Join-Path $buildFolder -ChildPath "Help/$moduleName/en-us"
     $outputFolder
     Import-Module ./$moduleName -Force
     if ($Create.IsPresent)
@@ -34,7 +34,7 @@ if (-not $Create.IsPresent)
     Update-MarkdownHelpModule -Path $outPath -RefreshModulePage -AlphabeticParamsOrder
 }
 
-foreach ($md in (Get-ChildItem -Filter *.md -Recurse -Path (Join-Path -Path $location -ChildPath Help)))
+foreach ($md in (Get-ChildItem -Filter *.md -Recurse -Path (Join-Path -Path $buildFolder -ChildPath Help)))
 {
     if (-not (Get-Command -ErrorAction SilentlyContinue -Name $md.BaseName)) { continue }
 
@@ -45,19 +45,19 @@ foreach ($md in (Get-ChildItem -Filter *.md -Recurse -Path (Join-Path -Path $loc
     $content | Set-Content -Path $md.FullName
 }
 
-$mkdocs = Join-Path -Path $location -ChildPath mkdocs.yml -Resolve -ErrorAction Stop
+$mkdocs = Join-Path -Path $buildFolder -ChildPath mkdocs.yml -Resolve -ErrorAction Stop
 $mkdocsContent = Get-Content -Raw -Path $mkdocs | ConvertFrom-Yaml
 
 # Update Sample Scripts help content
 $null = ($mkdocsContent.nav | Where-Object {$_.Keys -contains 'Sample scripts'})['Sample scripts'] = New-Object System.Collections.ArrayList
-foreach ($folder in (Get-ChildItem -Path (Join-Path -Path $location -ChildPath 'LabSources\SampleScripts') -Directory))
+foreach ($folder in (Get-ChildItem -Path (Join-Path -Path $buildFolder -ChildPath 'LabSources\SampleScripts') -Directory))
 {
     $folderObject = @{ $folder.Name = New-Object System.Collections.ArrayList}
     foreach ($sample in $folder.GetFiles('*.ps1'))
     {
         $mdRelativePathMkDocs = "Wiki/SampleScripts/$($folder.BaseName)/en-us/$($sample.BaseName).md"
         $mdRelativePath = "Help/Wiki/SampleScripts/$($folder.BaseName)/en-us/$($sample.BaseName).md"
-        $mdFullPath = Join-Path -Path $location -ChildPath $mdRelativePath
+        $mdFullPath = Join-Path -Path $buildFolder -ChildPath $mdRelativePath
         $scriptObject = @{
             $sample.Name = $mdRelativePathMkDocs
         }
