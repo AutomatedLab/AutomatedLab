89e3e61ff5551d5f362b220a477a62b62b0195c6
Dave Kettmann <david.kettmann@gmail.com>
2025-08-21 12:26:27 -0400

    Updated Invoke-LabPester.ps1 to now check the CustomRole directory for each detected CustomRole used for `tests.ps1` files.

--- AutomatedLabTest/functions/Invoke-LabPester.ps1
+++ AutomatedLabTest/functions/Invoke-LabPester.ps1
@@ -1,5 +1,4 @@
-﻿function Invoke-LabPester
-{
+﻿function Invoke-LabPester {
     [CmdletBinding(DefaultParameterSetName = 'ByLab')]
     param
     (
@@ -21,37 +20,39 @@
         $OutputFile
     )
 
-    process
-    {
-        if (-not $Lab)
-        {
+    process {
+        if (-not $Lab) {
             $Lab = Import-Lab -Name $LabName -ErrorAction Stop -NoDisplay -NoValidation -PassThru
         }
 
         $global:pesterLab = $Lab # No parameters in Pester v5 yet
         $configuration = [PesterConfiguration]::Default
-        $configuration.Run.Path = Join-Path -Path $PSCmdlet.MyInvocation.MyCommand.Module.ModuleBase -ChildPath 'tests'
+        $config_paths = @() # Setup the array to pass to $Configuration.Run.Path
+        $config_paths += Join-Path -Path $PSCmdlet.MyInvocation.MyCommand.Module.ModuleBase -ChildPath 'tests' # This is for the built-in Roles
         $configuration.Run.PassThru = $PassThru.IsPresent
         [string[]]$tags = 'General'
         
-        if ($Lab.Machines.Roles.Name)
-        {
+        if ($Lab.Machines.Roles.Name) {
             $tags += $Lab.Machines.Roles.Name
         }
-        if ($Lab.Machines.PostInstallationActivity | Where-Object IsCustomRole)
-        {
+        if ($Lab.Machines.PostInstallationActivity | Where-Object IsCustomRole) {
             $tags += ($Lab.Machines.PostInstallationActivity | Where-Object IsCustomRole).RoleName
+            foreach ( $role in (($Lab.Machines.PostInstallationActivity | Where-Object IsCustomRole).RoleName)) {
+                $config_paths += Join-Path -Path $global:LabSources\CustomRoles -ChildPath $role
+            }
         }
-        if ($Lab.Machines.PreInstallationActivity | Where-Object IsCustomRole)
-        {
+        if ($Lab.Machines.PreInstallationActivity | Where-Object IsCustomRole) {
             $tags += ($Lab.Machines.PreInstallationActivity | Where-Object IsCustomRole).RoleName
+            foreach ( $role in (($Lab.Machines.PreInstallationActivity | Where-Object IsCustomRole).RoleName)) {
+                $config_paths += Join-Path -Path $global:LabSources\CustomRoles -ChildPath $role
+            }
         }
-
+        $configuration.Run.Path = $config_paths
         $configuration.Filter.Tag = $tags
         $configuration.Should.ErrorAction = 'Continue'
         $configuration.TestResult.Enabled = $true
-        if ($OutputFile)
-        {
+        
+        if ($OutputFile) {
             $configuration.TestResult.OutputPath = $OutputFile
         }
         $configuration.Output.Verbosity = $Show
