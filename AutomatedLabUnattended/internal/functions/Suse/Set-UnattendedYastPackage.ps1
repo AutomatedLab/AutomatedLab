829ebf8baa6b5cf397460e98697d7be2a346abd7
Jan-Hendrik <nyanhp@users.noreply.github.com>
2025-08-22 13:28:57 +0200

    Ensure no duplicates

--- AutomatedLabUnattended/internal/functions/Suse/Set-UnattendedYastPackage.ps1
+++ AutomatedLabUnattended/internal/functions/Suse/Set-UnattendedYastPackage.ps1
@@ -6,7 +6,13 @@
 
     $packagesNode = $script:un.SelectSingleNode('/un:profile/un:software/un:packages', $script:nsm)
     $patternsNode = $script:un.SelectSingleNode('/un:profile/un:software/un:patterns', $script:nsm)
+
     foreach ($p in $Package) {
+        if ($p -replace '^pattern_', '' -in $patternsNode.ChildNodes.InnerText -or $p -in $packagesNode.ChildNodes.InnerText) {
+            Write-Verbose "Package or pattern '$p' already exists in the unattended Yast configuration."
+            continue
+        }
+
         if ($p -match "^pattern_") {
             $patternNode = $script:un.CreateElement('pattern', $script:nsm.LookupNamespace('un'))
             $patternNode.InnerText = $p
