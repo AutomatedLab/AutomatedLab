8e9f04108ca475914c1fe90be331c568f967e134
Jan-Hendrik <nyanhp@users.noreply.github.com>
2025-10-24 13:34:13 +0200

    Reliably set DNS search domain

--- AutomatedLabUnattended/internal/functions/RedHat/Set-UnattendedKickstartDomain.ps1
+++ AutomatedLabUnattended/internal/functions/RedHat/Set-UnattendedKickstartDomain.ps1
@@ -1,5 +1,4 @@
-﻿function Set-UnattendedKickstartDomain
-{
+﻿function Set-UnattendedKickstartDomain {
 	param (
 		[Parameter(Mandatory = $true)]
 		[string]$DomainName,
@@ -12,15 +11,29 @@
 
 		[Parameter()]
 		[string]$OrganizationalUnit
-    )
+	)
 
-	if ($OrganizationalUnit)
-	{
+	if ($OrganizationalUnit) {
 		$script:un.Add(("realm join --computer-ou='{2}' --one-time-password='{0}' {1}" -f $Password, $DomainName, $OrganizationalUnit))
 
 	}
-	else
-	{
+	else {
 		$script:un.Add(("realm join --one-time-password='{0}' {1}" -f $Password, $DomainName))
 	}
+
+	$existingLine = $script:un | Where-Object { $_ -match 'network' }
+
+	if ($existingLine -like '*--ipv4-dns-search*') {
+		$index = $script:un.IndexOf($existingLine)
+		$script:un[$index] = $existingLine -replace 'ipv4-dns-search=[\w\.]+', "--ipv4-dns-search=$DomainName"
+		return
+	}
+
+	if ($existingLine) {
+		$index = $script:un.IndexOf($existingLine)
+		$script:un[$index] = '{0} {1}' -f $existingLine, "--ipv4-dns-search=$DomainName"
+		return
+	}
+
+	$script:un.Add("network --ipv4-dns-search=$DomainName")
 }
