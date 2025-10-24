39ff460628b6eb51fec698d3f02772bd78cb01cf
Jan-Hendrik <nyanhp@users.noreply.github.com>
2025-10-24 14:06:23 +0200

    Reliably set DNS search domain

--- AutomatedLabUnattended/internal/functions/RedHat/Set-UnattendedKickstartIpSettings.ps1
+++ AutomatedLabUnattended/internal/functions/RedHat/Set-UnattendedKickstartIpSettings.ps1
@@ -26,11 +26,20 @@
 
     $configurationItem += if ($DnsServers)
     {
-        ' --nameserver={0}' -f ($DnsServers.AddressAsString -join ',')
+        ' --nameserver={0} --ipv4-dns-search={1}' -f ($DnsServers.AddressAsString -join ','), $DnsDomain
     }
     else
     {
         ' --nodns'
+    }
+
+     $existingLine = $script:un | Where-Object { $_ -match 'network' }
+
+    if ($existingLine -like '*bootproto*') {
+        $index = $script:un.IndexOf($existingLine)
+        $null = $existingLine -match '(?<HostName>--hostname=\w+)'
+        $script:un[$index] = '{0} {1}' -f $configurationItem, $Matches.HostName
+        return
     }
 
     $script:un.Add($configurationItem)
