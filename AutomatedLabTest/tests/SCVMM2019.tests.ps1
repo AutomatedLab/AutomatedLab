b69bb4c78379da408bfd80766009318650cfafac
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2025-11-16 17:18:50 +0100

    Update tests with new path logic

--- AutomatedLabTest/tests/SCVMM2019.tests.ps1
+++ AutomatedLabTest/tests/SCVMM2019.tests.ps1
@@ -9,9 +9,10 @@ Context "Role deployment successful" {
             It "[$vm] Should have SCVMM 2019 installed" -TestCases @{
                 vm = $vm
             } {
-                $whichIni = if ($vm.Roles.Properties.ContainsKey('SkipServer')) {'c:\Console.ini'} else {'C:\Server.ini'}
-                Invoke-LabCommand -ComputerName $vm -NoDisplay -Variable (Get-Variable whichIni) -PassThru -ScriptBlock {
-                    $path = (Get-Content -Path $whichIni -ErrorAction SilentlyContinue | Where-Object {$_ -like 'ProgramFiles*'}) -split '\s*=\s*' | Select-Object -Last 1
+                $whichIni = if ($vm.Roles.Properties.ContainsKey('SkipServer')) {'Console.ini'} else {'Server.ini'}
+                Invoke-LabCommand -ComputerName $vm -NoDisplay -Variable (Get-Variable whichIni, AL_DeployDebugFolder) -PassThru -ScriptBlock {
+                    $deployDebug =  (Get-Item -Path $ExecutionContext.InvokeCommand.ExpandString($AL_DeployDebugFolder)).FullName
+                    $path = (Get-Content -Path (Join-Path $deployDebug $whichIni) -ErrorAction SilentlyContinue | Where-Object {$_ -like 'ProgramFiles*'}) -split '\s*=\s*' | Select-Object -Last 1
                     Test-Path -Path $path
                 } | Should -Be $true
             }
