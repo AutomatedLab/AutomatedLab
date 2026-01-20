d9eebd97fe6180f72ec02a1ced0d53aaa2d7a541
Raimund Andree <r.andree@live.com>
2026-01-20 17:21:30 +0100

    Last months work

--- AutomatedLabWorker/functions/ProxmoxWorkerVirtualMachines/Start-LWProxmoxVM.ps1
+++ AutomatedLabWorker/functions/ProxmoxWorkerVirtualMachines/Start-LWProxmoxVM.ps1
@@ -1,5 +1,6 @@
-function Start-LWProxmoxVM {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseCompatibleCmdlets", "", Justification = "Not relevant on Linux")]
+function Start-LWProxmoxVM
+{
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseCompatibleCmdlets", "", Justification="Not relevant on Linux")]
     param (
         [Parameter(Mandatory)]
         [string[]]$ComputerName,
@@ -22,20 +23,25 @@ function Start-LWProxmoxVM {
 
     $vms = Get-LWProxmoxVM
 
-    foreach ($vm in (Get-LabVM -ComputerName $ComputerName -IncludeLinux | Where-Object SkipDeployment -eq $false)) {
+    foreach ($vm in (Get-LabVM -ComputerName $ComputerName -IncludeLinux | Where-Object SkipDeployment -eq $false))
+    {
         $vmid = $vms | where-object { $_.Name -eq $vm.ResourceName } | Select-Object -ExpandProperty VmId
 
-        try {
+        try
+        {
             $proxmoxVm = Get-LWProxmoxVM -Name $vm
-            if ($proxmoxVm.status -eq 'running' -and $proxmoxVm.CurrentStatus.qmpstatus -eq 'running') {
+            if ($proxmoxVm.status -eq 'running' -and $proxmoxVm.CurrentStatus.qmpstatus -eq 'running')
+            {
                 Write-PSFMessage -Message "Proxmox machine '$vm' is already running. Skipping start." -Level 'Verbose'
                 continue
             }
 
-            if ($proxmoxVm.CurrentStatus.qmpstatus -eq 'paused') {
+            if ($proxmoxVm.CurrentStatus.qmpstatus -eq 'paused')
+            {
                 Write-PSFMessage -Message "Resuming Proxmox machine '$vm' from paused state." -Level 'Verbose'
-                $result = Resume-PveQemu -Node $global:proxmoxNode -Vmid $vmid
-                if ($result.StatusCode -ne 200) {
+                $result = Resume-PveQemu -Node $proxmoxVm.node -Vmid $vmid
+                if ($result.StatusCode -ne 200)
+                {
                     Write-Error "Could not resume Proxmox machine '$vm': The error was '$($result.StatusCode)'" -ErrorAction Stop
                 }
                 continue
@@ -43,34 +49,40 @@ function Start-LWProxmoxVM {
 
             $result = Start-PveVm -VmIdOrName $vmid
 
-            if ($result.StatusCode -ne 200) {
+            if ($result.StatusCode -ne 200)
+            {
                 Write-Error "Could not start Proxmox machine '$vm': The error was '$($result.StatusCode)'" -ErrorAction Stop
             }
 
             $values = @{
                 status = 'stopped'
             }
-            $result = Wait-LWProxmoxTasksStatus -Upid $result.Response.data -DesiredValues $values -TimeoutInSeconds 600
-            if ($result -ne 'OK') {
+            $result = Wait-LWProxmoxTasksStatus -Upid $result.Response.data -Node $proxmoxVm.node -DesiredValues $values -TimeoutInSeconds 600
+            if ($result -ne 'OK')
+            {
                 Write-Error -Message "Could not start Proxmox machine '$vm': Timeout waiting for status change." -ErrorAction Stop
             }
         }
-        catch {
+        catch
+        {
             Write-Error -Message "Could not start Proxmox machine '$vm': $($_.Exception.Message)" -Exception $_.Exception -ErrorAction Stop
         }
 
-        if ($vm.OperatingSystemType -eq 'Linux') {
+        if ($vm.OperatingSystemType -eq 'Linux')
+        {
             Write-PSFMessage -Message "Skipping the wait period for '$vm' as it is a Linux system"
             continue
         }
 
-        if ($DelayBetweenComputers -and $vm -ne $ComputerName[-1]) {
+        if ($DelayBetweenComputers -and $vm -ne $ComputerName[-1])
+        {
             $job = Start-Job -Name 'Start-LWHypervVM - DelayBetweenComputers' -ScriptBlock { Start-Sleep -Seconds $Using:DelayBetweenComputers }
             Wait-LWLabJob -Job $job -NoNewLine:$NoNewLine -ProgressIndicator $ProgressIndicator -Timeout 15 -NoDisplay
         }
     }
 
-    if ($PostDelaySeconds) {
+    if ($PostDelaySeconds)
+    {
         $job = Start-Job -Name 'Start-LWHypervVM - Post Delay' -ScriptBlock { Start-Sleep -Seconds $Using:PostDelaySeconds }
         Wait-LWLabJob -Job $job -NoNewLine:$NoNewLine -ProgressIndicator $ProgressIndicator -Timeout 15 -NoDisplay
     }
--- AutomatedLabWorker/functions/VirtualMachines/Checkpoint-LWProxmoxVM.ps1
+++ AutomatedLabWorker/functions/VirtualMachines/Checkpoint-LWProxmoxVM.ps1
@@ -1,4 +1,5 @@
-function Checkpoint-LWProxmoxVM {
+function Checkpoint-LWProxmoxVM
+{
     [Cmdletbinding()]
     Param
     (
@@ -14,29 +15,34 @@ function Checkpoint-LWProxmoxVM {
 
     $lab = Get-Lab
 
-    $jobs = foreach ($machine in $ComputerName) {
+    $jobs = foreach ($machine in $ComputerName)
+    {
         $vm = Get-LWProxmoxVM -Name $machine -ErrorAction SilentlyContinue
-        if (-not $vm) {
+        if (-not $vm)
+        {
             Write-ScreenInfo -Message "Proxmox VM '$machine' could not be found. Skipping snapshot." -Type Warning
             continue
         }
 
         $existingSnapshot = Get-LWProxmoxVmSnapshot -ComputerName $machine -SnapshotName $SnapshotName
-        if ($existingSnapshot) {
+        if ($existingSnapshot)
+        {
             Write-ScreenInfo -Message "Snapshot '$SnapshotName' for '$machine' already exists as '$($existingSnapshot.SnapshotName)'. Not creating it again." -Type Warning
             continue
         }
 
         $vmSnapshotName = '{0}_{1}' -f $machine, $SnapshotName
-        $result = New-PveNodesQemuSnapshot -Node $global:proxmoxNode -Description 'Created by AutomatedLab' -Snapname $vmSnapshotName -Vmid $vm.VmId
-        if ($result.StatusCode -ne 200) {
+        $result = New-PveNodesQemuSnapshot -Node $vm.node -Description 'Created by AutomatedLab' -Snapname $vmSnapshotName -Vmid $vm.VmId
+        if ($result.StatusCode -ne 200)
+        {
             Write-Error "Could not create snapshot '$SnapshotName' for Proxmox machine '$machine': The error was '$($result.StatusCode)'" -ErrorAction Stop
         }
         $values = @{
             status = 'stopped'
         }
-        $taskResult = Wait-LWProxmoxTasksStatus -Upid $result.Response.data -DesiredValues $values -TimeoutInSeconds 600
-        if ($taskResult -ne 'OK') {
+        $taskResult = Wait-LWProxmoxTasksStatus -Upid $result.Response.data -Node $vm.node -DesiredValues $values -TimeoutInSeconds 600
+        if ($taskResult -ne 'OK')
+        {
             Write-Error -Message "Failed to create snapshot '$SnapshotName' for Proxmox machine '$machine'. Task did not complete successfully with the error '$taskResult'." -TargetObject $machine
         }
     }
