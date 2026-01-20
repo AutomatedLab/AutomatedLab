cb1066ce38171d829e5028840ab06bc4c43e1ad9
Raimund Andree <r.andree@live.com>
2026-01-20 17:02:38 +0100

    Refactor Get-LWProxmoxVM function to improve parameter handling and caching logic

--- AutomatedLabWorker/functions/ProxmoxWorkerVirtualMachines/Get-LWProxmoxVM.ps1
+++ AutomatedLabWorker/functions/ProxmoxWorkerVirtualMachines/Get-LWProxmoxVM.ps1
@@ -1,4 +1,5 @@
-function Get-LWProxmoxVM {
+function Get-LWProxmoxVM
+{
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCmdlets', '', Justification = 'Not relevant on Linux')]
     [CmdletBinding()]
     param
@@ -9,59 +10,137 @@ function Get-LWProxmoxVM {
         $ComputerName,
 
         [Parameter()]
+        [object[]]
+        $Node,
+
+        [Parameter()]
         [switch]
         $NoError,
 
         [Parameter()]
         [switch]
-        $IncludeTemplates
+        $IncludeTemplates,
+
+        [Parameter()]
+        [switch]
+        $NoCache,
+
+        [Parameter()]
+        [switch]
+        $NoStatusCurrent
     )
 
     Write-LogFunctionEntry
 
-    if (-not (Test-LabProxmoxConnection)) {
+    if (-not (Test-LabProxmoxConnection))
+    {
         Write-Error 'There is no connection to the Proxmox cluster.' -ErrorAction Stop
         return
     }
 
-    $vms = Get-PveNodesQemu -Node $global:proxmoxNode
-
-    if ($vms.StatusCode -ne 200) {
-        Write-Error "Failed to retrieve VM templates from Proxmox node '$($Node.node)': $($vms.ReasonPhrase)"
-        return
+    if (-not $Node)
+    {
+        $Node = Get-LWProxmoxNode | Select-Object -ExpandProperty node
+    }
+    else
+    {
+        $Node = Get-LWProxmoxNode -Name $Node | Select-Object -ExpandProperty node
     }
 
-    $vms = if ($IncludeTemplates) {
-        $vms.Response.data
-    }
-    else {
-        $vms.Response.data | Where-Object { $_.template -ne 1 }
+    if ($null -eq $script:proxmoxVmCache)
+    {
+        $script:proxmoxVmCache = @{}
     }
 
-    foreach ($vm in $vms) {
-        $currentStatus = Get-PveNodesQemuStatusCurrent -Node $proxmoxNode -Vmid $vm.vmid
-        $vm | Add-Member -Name CurrentStatus -MemberType NoteProperty -Value $currentStatus.Response.data
-        $vm.tags = $vm.tags -split ';'
+    if ($NoCache)
+    {
+        Write-ScreenInfo -Message "Retrieving VM(s) from Proxmox node(s) '$($Node -join ', ')'" -Type Info -NoNewLine -TaskStart
     }
+
+    $vms = foreach ($n in $Node)
+    {
+        if ($NoCache.IsPresent -or -not $script:proxmoxVmCache.ContainsKey($n))
+        {
+            $script:proxmoxVmCache.$n = $null
+
+            Write-ScreenInfo -Message "Retrieving VM(s) from Proxmox node '$($n)'" -Type Verbose
+            Write-ScreenInfo -Message '.' -Type Info -NoNewLine
+            $result = Get-PveNodesQemu -Node $n
+            if ($result.StatusCode -ne 200)
+            {
+                Write-Error "Failed to retrieve VM(s) from Proxmox node '$($n)': $($result.ReasonPhrase)"
+                return
+            }
+
+            $result = $result.response.data
+            $result | Add-Member -Name node -MemberType NoteProperty -Value $n
+
+            foreach ($vm in $result)
+            {
+                if (-not $NoStatusCurrent)
+                {
+                    Write-ScreenInfo -Message "Retrieving current status for VM '$($vm.name)' on Proxmox node '$n'" -Type Verbose
+                    $currentStatus = Get-PveNodesQemuStatusCurrent -Node $n -Vmid $vm.vmid
+                    $vm | Add-Member -Name CurrentStatus -MemberType NoteProperty -Value $currentStatus.Response.data
+                }
+                if (-not [string]::IsNullOrEmpty($vm.tags))
+                {
+                    $vm.tags = $vm.tags -split ';'
+                }
+            }
+            $script:proxmoxVmCache.$n = $result
+            $result
+        }
+        else
+        {
+            Write-ScreenInfo -Message "Using cached Proxmox VM information for node '$n'" -Type Verbose
+            $script:proxmoxVmCache.$n
+        }
+    }
+    Write-ScreenInfo -Message 'done.' -Type Info -TaskEnd
 
     [object[]]$vms = $vms
 
+    if ($ComputerName.Count -gt 0)
+    {
+        $vms = $vms | Where-Object { $_.Name -in $ComputerName }
+    }
+
     $vms = $vms | Sort-Object -Unique -Property Name
 
-    if ($ComputerName.Count -gt 0) {
-        return $vms | Where-Object { $_.Name -in $ComputerName }
-    }
-
-    if (-not $NoError.IsPresent -and $ComputerName.Count -gt 0 -and -not $vm) {
-        Write-Error -Message "No virtual machine '$ComputerName' found"
+    if (-not $NoError.IsPresent -and $ComputerName.Count -gt 0 -and -not $vms)
+    {
+        Write-Error -Message "Virtual machine '$ComputerName' not found on node(s) '$($Node -join ', ')'"
         return
     }
 
-    if ($vms.Count -eq 0) {
+    if ($vms.Count -eq 0)
+    {
         return
     }
 
-    $vms
+    if ($IncludeTemplates)
+    {
+        if ($ComputerName.Count -gt 0)
+        {
+            return $vms | Where-Object { $_.Name -in $ComputerName }
+        }
+        else
+        {
+            $vms
+        }
+    }
+    else
+    {
+        if ($ComputerName.Count -gt 0)
+        {
+            return $vms | Where-Object { $_.Name -in $ComputerName -and $_.template -ne 1 }
+        }
+        else
+        {
+            return $vms | Where-Object { $_.template -ne 1 }
+        }
+    }
 
     Write-LogFunctionExit
 }
