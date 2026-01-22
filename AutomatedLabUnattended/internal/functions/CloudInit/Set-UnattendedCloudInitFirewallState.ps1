a1d49f137c7f6e5010a5f6c9922deecd65567e1e
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2026-01-22 14:39:04 +0100

    Ensure UFW does not cause subiqity error

--- AutomatedLabUnattended/internal/functions/CloudInit/Set-UnattendedCloudInitFirewallState.ps1
+++ AutomatedLabUnattended/internal/functions/CloudInit/Set-UnattendedCloudInitFirewallState.ps1
@@ -6,6 +6,6 @@
 		[boolean]$State
 	)
 
-    $script:un['autoinstall']['late-commands'] += 'curtin in-target --target=/target -- ufw enable'
-    $script:un['autoinstall']['late-commands'] += 'curtin in-target --target=/target -- ufw allow 22'
+    $script:un['autoinstall']['late-commands'] += 'curtin in-target --target=/target -- ufw enable 2>/dev/null || true'
+    $script:un['autoinstall']['late-commands'] += 'curtin in-target --target=/target -- ufw allow 22 2>/dev/null || true'
 }
