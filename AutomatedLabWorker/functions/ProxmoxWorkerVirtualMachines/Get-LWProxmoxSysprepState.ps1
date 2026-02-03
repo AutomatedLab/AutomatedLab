e2b54e7a9bcf1e44191a3f5384f2a2e97c84b6e9
Raimund Andree <r.andree@live.com>
2026-02-03 14:20:44 +0100

    Update documentation for Get-LWProxmoxVMSysprepState function

--- AutomatedLabWorker/functions/ProxmoxWorkerVirtualMachines/Get-LWProxmoxSysprepState.ps1
+++ AutomatedLabWorker/functions/ProxmoxWorkerVirtualMachines/Get-LWProxmoxSysprepState.ps1
@@ -1,4 +1,27 @@
 function Get-LWProxmoxVMSysprepState {
+    <#
+    .SYNOPSIS
+        Gets the sysprep state of Proxmox virtual machines.
+
+    .DESCRIPTION
+        This function retrieves the Windows sysprep state from the registry of one or more Proxmox virtual machines.
+        It queries the ImageState value from HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State to determine
+        the current sysprep status of each machine.
+
+    .PARAMETER ComputerName
+        The name(s) of the computer(s) to query for sysprep state. This parameter accepts an array of strings
+        representing the computer names of Proxmox virtual machines.
+
+    .EXAMPLE
+        Get-LWProxmoxVMSysprepState -ComputerName 'Server01'
+
+        Gets the sysprep state for a single virtual machine named Server01.
+
+    .EXAMPLE
+        Get-LWProxmoxVMSysprepState -ComputerName 'Server01', 'Server02', 'Server03'
+
+        Gets the sysprep state for multiple virtual machines.
+    #>
     param (
         [Parameter(Mandatory)]
         [string[]]$ComputerName
