19c70233642b2dd54ca323183a4d831f3848c6e0
Raimund Andree <r.andree@live.com>
2026-05-26 15:51:07 +0200

    feat(tests): update test for Proxmox to ensure correct machine count retrieval

--- AutomatedLabTest/tests/00General.tests.ps1
+++ AutomatedLabTest/tests/00General.tests.ps1
@@ -1,18 +1,20 @@
 ﻿Describe "[$($Lab.Name)] General" -Tag General {
     Context "Lab deployment successful" {
-            It "[<LabName>] Should return the correct amount of machines" -TestCases @(@{Lab = $Lab; LabName = $Lab.Name}) {
-                $machines = if ($Lab.DefaultVirtualizationEngine -eq 'Azure')
-                {
-                    Get-LWAzureVm -ComputerName (Get-LabVm -IncludeLinux | Where-Object SkipDeployment -eq $false).ResourceName
-                }
-                elseif ($Lab.DefaultVirtualizationEngine -eq 'HyperV')
-                {
-                    Get-LWHyperVVm -Name (Get-LabVm -IncludeLinux | Where-Object SkipDeployment -eq $false).ResourceName
-                }
-
-                $machines.Count | Should -Be $($lab.Machines | Where-Object SkipDeployment -eq $false).Count
+        It "[<LabName>] Should return the correct amount of machines" -TestCases @(@{Lab = $Lab; LabName = $Lab.Name}) {
+            $machines = if ($Lab.DefaultVirtualizationEngine -eq 'Azure')
+            {
+                Get-LWAzureVm -ComputerName (Get-LabVm -IncludeLinux | Where-Object SkipDeployment -eq $false).ResourceName
             }
+            elseif ($Lab.DefaultVirtualizationEngine -eq 'HyperV')
+            {
+                Get-LWHyperVVm -Name (Get-LabVm -IncludeLinux | Where-Object SkipDeployment -eq $false).ResourceName
+            }
+            elseif ($Lab.DefaultVirtualizationEngine -eq 'Proxmox')
+            {
+                Get-LWProxmoxVM -ComputerName (Get-LabVm -IncludeLinux | Where-Object SkipDeployment -eq $false).ResourceName
+            }
+
+            @($machines).Count | Should -Be $($lab.Machines | Where-Object SkipDeployment -eq $false).Count
         }
     }
-    
-    
+}
