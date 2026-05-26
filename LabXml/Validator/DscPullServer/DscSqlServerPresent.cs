a81438346548e5fd9861157daeea74d2b7688aae
Raimund Andree <r.andree@live.com>
2026-05-26 20:38:07 +0200

    fix: update DSC Pull Server lab validator to support SQL Server 2022 and 2025

--- LabXml/Validator/DscPullServer/DscSqlServerPresent.cs
+++ LabXml/Validator/DscPullServer/DscSqlServerPresent.cs
@@ -17,7 +17,7 @@ namespace AutomatedLab.Validator.FailoverCluster
         {
             var role = Roles.DSCPullServer;
             var machines = lab.Machines.Where(m => m.Roles.Where(r => r.Name == role).Count() > 0);
-            var sqlServers = lab.Machines.Where(m => m.Roles.Where(r => r.Name == Roles.SQLServer2016 || r.Name == Roles.SQLServer2017 || r.Name == Roles.SQLServer2019).Count() > 0);
+            var sqlServers = lab.Machines.Where(m => m.Roles.Where(r => r.Name == Roles.SQLServer2016 || r.Name == Roles.SQLServer2017 || r.Name == Roles.SQLServer2019 || r.Name == Roles.SQLServer2022 || r.Name == Roles.SQLServer2025).Count() > 0);
 
             foreach (var machine in machines)
             {
@@ -31,7 +31,7 @@ namespace AutomatedLab.Validator.FailoverCluster
                         {
                             yield return new ValidationMessage
                             {
-                                Message = string.Format("The database server for the DSC Pull Server role is '{0}' but there is no SQL Server 2016 or 2017 defined inthe lab with that name", targetedSqlServer),
+                                Message = string.Format("The database server for the DSC Pull Server role is '{0}' but there is no supported SQL Server (2016, 2017, 2019, 2022 or 2025) defined in the lab with that name", targetedSqlServer),
                                 Type = MessageType.Error,
                                 TargetObject = machine.Name
                             };
